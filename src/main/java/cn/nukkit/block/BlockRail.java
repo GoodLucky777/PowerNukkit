13708cde97591e9f30c8a609e50c12bc28417e5a
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-04 21:09:22 +0900

    Oops

--- src/main/java/cn/nukkit/block/BlockRail.java
+++ src/main/java/cn/nukkit/block/BlockRail.java
@@ -72,7 +72,7 @@ public class BlockRail extends BlockFlowable implements Faceable {
     public int onUpdate(int type) {
         if (type == Level.BLOCK_UPDATE_NORMAL) {
             Optional<BlockFace> ascendingDirection = this.getOrientation().ascendingDirection();
-            if (!checkCanBePlace(this.down) || (ascendingDirection.isPresent() && !checkCanBePlace(this.getSide(ascendingDirection.get())))) {
+            if (!checkCanBePlace(this.down()) || (ascendingDirection.isPresent() && !checkCanBePlace(this.getSide(ascendingDirection.get())))) {
                 this.getLevel().useBreakOn(this);
                 return Level.BLOCK_UPDATE_NORMAL;
             }
