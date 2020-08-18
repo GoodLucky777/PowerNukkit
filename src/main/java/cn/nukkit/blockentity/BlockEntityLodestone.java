ebed4f637403378ffd8e4c44befa311be7a3fdcf
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-18 20:47:21 +0900

    Create BlockEntityLodestone.java

---
+++ src/main/java/cn/nukkit/blockentity/BlockEntityLodestone.java
@@ -0,0 +1,66 @@
+package cn.nukkit.blockentity;
+
+import cn.nukkit.block.Block;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.nbt.tag.CompoundTag;
+
+/**
+ * @author good777LUCKY
+ */
+public class BlockEntityLodestone extends BlockEntitySpawnable {
+
+    public Integer trackingHandle;
+    
+    public BlockEntityLodestone(FullChunk chunk, CompoundTag nbt) {
+        super(chunk, nbt);
+    }
+    
+    @Override
+    protected void initBlockEntity() {
+        this.trackingHandle = null;
+        super.initBlockEntity();
+    }
+    
+    @Override
+    public boolean isBlockEntityValid() {
+        return this.getBlock().getId() == Block.LODESTONE;
+    }
+    
+    @Override
+    public void saveNBT() {
+        super.saveNBT();
+        if (this.trackingHandle != null) {
+            this.namedTag.putInt("trackingHandle", this.trackingHandle);
+        }
+    }
+    
+    @Override
+    public String getName() {
+        return "Lodestone";
+    }
+    
+    public Integer getTrackingHandle() {
+        if (this.trackingHandle == null) {
+            return null;
+        }
+        return this.namedTag.getInt("trackingHandle");
+    }
+    
+    public void setTrackingHandle(int trackingHandle) {
+        this.trackingHandle = trackingHandle;
+        this.namedTag.putInt("trackingHandle", trackingHandle);
+    }
+    
+    @Override
+    public CompoundTag getSpawnCompound() {
+        Compound compound = new Compound()
+            .putString("id", BlockEntity.LODESTONE)
+            .putInt("x", (int) this.x)
+            .putInt("y", (int) this.y)
+            .putInt("z", (int) this.z);
+        if (trackingHandle != null) {
+            compound.putInt("trackingHandle", getTrackingHandle());
+        }
+        return compound;
+    }
+}
