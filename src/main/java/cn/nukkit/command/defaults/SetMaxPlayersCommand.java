e24c08ac768ffef3a409df3f7fafca6f9bf9c6cb
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-20 21:30:52 +0900

    Create SetMaxPlayersCommand.java

---
+++ src/main/java/cn/nukkit/command/defaults/SetMaxPlayersCommand.java
@@ -0,0 +1,50 @@
+package cn.nukkit.command.defaults;
+
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.data.CommandParameter;
+import cn.nukkit.lang.TranslationContainer;
+import cn.nukkit.level.GameRule;
+import cn.nukkit.level.GameRules;
+import cn.nukkit.level.Level;
+
+/**
+ * @author good777LUCKY
+ */
+public class SetMaxPlayersCommand extends VanillaCommand {
+
+    public SetMaxPlayersCommand(String name) {
+        super(name, "%nukkit.command.setmaxplayers.description", "%commands.setmaxplayers.usage");
+        this.setPermission("nukkit.command.setdayplayers");
+        this.commandParameters.clear();
+        this.commandParameters.put("default", new CommandParameter[]{
+                new CommandParameter("maxPlayers", CommandParamType.INT, false)
+        });
+    }
+    
+    @Override
+    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+        if (!this.testPermission(sender)) {
+            return true;
+        }
+        
+        if (args.length > 1 || args.length == 0) {
+            sender.sendMessage(new TranslationContainer("commands.generic.usage", this.usageMessage));
+            return false;
+        }
+        
+        int maxPlayers = 1;
+        
+        maxPlayers = Integer.parseInt(args[0]);
+        
+        if (maxPlayers < 1 || maxPlayers > 30 || maxPlayers < Server.getInstance().getOnlinePlayers().values) {
+            
+        }
+        
+        Server.getInstance().setMaxPlayers(maxPlayers);
+        
+        return true;
+    }
+}
