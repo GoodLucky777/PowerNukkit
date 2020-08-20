2dd70b309d44eb286b114d646fccf266153ddee0
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-20 14:58:21 +0900

    Update BambooJungleHillsBiome.java

--- src/main/java/cn/nukkit/level/biome/impl/jungle/BambooJungleHillsBiome.java
+++ src/main/java/cn/nukkit/level/biome/impl/jungle/BambooJungleHillsBiome.java
@@ -3,6 +3,7 @@ package cn.nukkit.level.biome.impl.jungle;
 /**
  * @author good777LUCKY
  */
+// TODO: Correct all values
 public class BambooJungleHillsBiome extends BambooJungleBiome {
 
     public BambooJungleHillsBiome() {
