e498cff2c9a7f1670aadd800472dbb163026f9d2
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-27 11:18:28 +0900

    Create BlockInfoUpdate2.java

---
+++ src/main/java/cn/nukkit/block/BlockInfoUpdate2.java
@@ -0,0 +1,50 @@
+package cn.nukkit.block;
+
+import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemTool;
+import cn.nukkit.utils.BlockColor;
+
+/**
+ * @author good777LUCKY
+ */
+public class BlockInfoUpdate2 extends BlockSolid {
+
+    public BlockInfoUpdate2() {
+        // Does Nothing
+    }
+    
+    @Override
+    public String getName() {
+        return "info_update2";
+    }
+    
+    @Override
+    public int getId() {
+        return INFO_UPDATE2;
+    }
+    
+    @Override
+    public double getHardness() {
+        return 0.5;
+    }
+    
+    @Override
+    public double getResistance() {
+        return 0.5;
+    }
+    
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_SWORD;
+    }
+    
+    @Override
+    public Item[] getDrops(Item item) {
+       return new Item[]{ toItem() };
+    }
+    
+    @Override
+    public BlockColor getColor() {
+        return BlockColor.DIRT_BLOCK_COLOR;
+    }
+}
