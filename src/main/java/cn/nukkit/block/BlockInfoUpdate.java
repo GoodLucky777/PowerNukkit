a9e3fd6e4c7a6a5ffd753e257dab35c32001004b
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-27 11:22:40 +0900

    Update BlockInfoUpdate.java

--- src/main/java/cn/nukkit/block/BlockInfoUpdate.java
+++ src/main/java/cn/nukkit/block/BlockInfoUpdate.java
@@ -30,7 +30,7 @@ public class BlockInfoUpdate extends BlockSolid {
     
     @Override
     public double getResistance() {
-        return 0.5;
+        return 0.5; // TODO: Correct Resistance
     }
     
     @Override
