668aa98a6cc7abd98bdf7f10b0da852d3f83133a
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-27 18:56:14 +0900

    Update BlockInfoUpdate.java

--- src/main/java/cn/nukkit/block/BlockInfoUpdate.java
+++ src/main/java/cn/nukkit/block/BlockInfoUpdate.java
@@ -1,6 +1,5 @@
 package cn.nukkit.block;
 
-import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.utils.BlockColor;
 
@@ -36,11 +35,6 @@ public class BlockInfoUpdate extends BlockSolid {
     @Override
     public int getToolType() {
         return ItemTool.TYPE_SWORD;
-    }
-    
-    @Override
-    public Item[] getDrops(Item item) {
-       return new Item[]{ toItem() };
     }
     
     @Override
