8b2e5203c354cc9c96913173caef60beadde5dd8
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-10-02 14:47:18 +0900

    Create EntityAgent.java

---
+++ src/main/java/cn/nukkit/entity/mob/EntityAgent.java
@@ -0,0 +1,59 @@
+package cn.nukkit.entity.mob;
+
+import cn.nukkit.event.entity.EntityDamageEvent;
+import cn.nukkit.inventory.ContainerInventory;
+import cn.nukkit.inventory.InventoryHolder;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.nbt.tag.CompoundTag;
+
+/**
+ * @author good777LUCKY
+ */
+public class EntityAgent extends EntityMob implements InventoryHolder {
+
+    public final static int NETWORK_ID = 56;
+    
+    protected ContainerInventory inventory;
+    
+    @Override
+    public int getNetworkId() {
+        return NETWORK_ID;
+    }
+    
+    public EntityAgent(FullChunk chunk, CompoundTag nbt) {
+        super(chunk, nbt);
+    }
+    
+    @Override
+    protected void initEntity() {
+        super.initEntity();
+        
+        this.setMaxHealth(Integer.MAX_VALUE);
+        this.setHealth(20); // TODO: Check health and max health
+        
+        this.setNameTagVisible(true);
+        this.setNameTagAlwaysVisible(true);
+        
+        this.fireProof = true;
+    }
+    
+    @Override
+    public float getWidth() {
+        return 0.6f;
+    }
+    
+    @Override
+    public float getHeight() {
+        return 0.93f;
+    }
+    
+    @Override
+    public String getName() {
+        return "Agent";
+    }
+    
+    @Override
+    public boolean attack(EntityDamageEvent source) {
+        return false;
+    }
+}
