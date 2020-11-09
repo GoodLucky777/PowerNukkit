7c662c89624cd081babadae24fe83b2288d6c5fd
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-11-09 16:49:45 +0900

    Create BambooStalkThickness.java

---
+++ src/main/java/cn/nukkit/blockproperty/value/BambooStalkThickness.java
@@ -0,0 +1,19 @@
+package cn.nukkit.blockproperty.value;
+
+import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.Since;
+
+/**
+ * @author good777LUCKY
+ */
+@PowerNukkitOnly
+@Since("1.4.0.0-PN")
+public enum BambooStalkThickness {
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    THIN,
+    
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    THICK;
+}
