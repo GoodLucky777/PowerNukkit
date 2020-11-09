05fc12fd50b57bd0f139239493820c07a495cb23
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-11-09 16:39:17 +0900

    Create BambooLeafSize.java

---
+++ src/main/java/cn/nukkit/blockproperty/value/BambooLeafSize.java
@@ -0,0 +1,23 @@
+package cn.nukkit.blockproperty.value;
+
+import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.Since;
+
+/**
+ * @author good777LUCKY
+ */
+@PowerNukkitOnly
+@Since("1.4.0.0-PN")
+public enum BambooLeafSize {
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    NO_LEAVES,
+    
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    SMALL_LEAVES,
+    
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    LARGE_LEAVES;
+}
