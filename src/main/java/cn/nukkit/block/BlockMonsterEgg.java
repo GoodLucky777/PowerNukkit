3936d3f0525f26645662ab001e29ab27d333ab00
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-12-18 11:46:08 +0900

    Upgrade Infested Blocks to new block state system

--- src/main/java/cn/nukkit/block/BlockMonsterEgg.java
+++ src/main/java/cn/nukkit/block/BlockMonsterEgg.java
@@ -1,16 +1,39 @@
 package cn.nukkit.block;
 
+import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.Since;
+import cn.nukkit.blockproperty.ArrayBlockProperty;
+import cn.nukkit.blockproperty.BlockProperties;
+import cn.nukkit.blockproperty.BlockProperty;
+import cn.nukkit.blockproperty.value.MonsterEggStoneType;
 import cn.nukkit.item.Item;
 
 public class BlockMonsterEgg extends BlockSolidMeta {
 
+    @Since("1.4.0.0-PN")
+    @PowerNukkitOnly
+    public static final BlockProperty<MonsterEggStoneType> MONSTER_EGG_STONE_TYPE = new ArrayBlockProperty<>(
+        "monster_egg_stone_type", true, MonsterEggStoneType.class
+    );
+    
+    @Since("1.4.0.0-PN")
+    @PowerNukkitOnly
+    public static final BlockProperties PROPERTIES = new BlockProperties(MONSTER_EGG_STONE_TYPE);
+    
+    @Deprecated
     public static final int STONE = 0;
+    @Deprecated
     public static final int COBBLESTONE = 1;
+    @Deprecated
     public static final int STONE_BRICK = 2;
+    @Deprecated
     public static final int MOSSY_BRICK = 3;
+    @Deprecated
     public static final int CRACKED_BRICK = 4;
+    @Deprecated
     public static final int CHISELED_BRICK = 5;
-
+    
+    @Deprecated
     private static final String[] NAMES = new String[]{
             "Stone",
             "Cobblestone",
@@ -19,7 +42,7 @@ public class BlockMonsterEgg extends BlockSolidMeta {
             "Cracked Stone Brick",
             "Chiseled Stone Brick"
     };
-
+    
     public BlockMonsterEgg() {
         this(0);
     }
@@ -32,7 +55,27 @@ public class BlockMonsterEgg extends BlockSolidMeta {
     public int getId() {
         return MONSTER_EGG;
     }
-
+    
+    @Since("1.4.0.0-PN")
+    @PowerNukkitOnly
+    @Nonnull
+    @Override
+    public BlockProperties getProperties() {
+        return PROPERTIES;
+    }
+    
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    public MonsterEggStoneType getMonsterEggStoneType() {
+        return getPropertyValue(MONSTER_EGG_STONE_TYPE);
+    }
+    
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    public void setMonsterEggStoneType(MonsterEggStoneType type) {
+        setPropertyValue(MONSTER_EGG_STONE_TYPE, type);
+    }
+    
     @Override
     public double getHardness() {
         return 0.75;
@@ -45,7 +88,7 @@ public class BlockMonsterEgg extends BlockSolidMeta {
 
     @Override
     public String getName() {
-        return NAMES[this.getDamage() > 5 ? 0 : this.getDamage()] + " Monster Egg";
+        return getMonsterEggStoneType().getEnglishName();
     }
 
     @Override
