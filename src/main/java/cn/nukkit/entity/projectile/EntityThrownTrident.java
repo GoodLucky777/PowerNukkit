bd19341a99145a312c1c8ff0a0d32f69b83d3807
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-12-23 08:45:56 +0900

    Fix import

--- src/main/java/cn/nukkit/entity/projectile/EntityThrownTrident.java
+++ src/main/java/cn/nukkit/entity/projectile/EntityThrownTrident.java
@@ -2,6 +2,8 @@ package cn.nukkit.entity.projectile;
 
 import cn.nukkit.Player;
 import cn.nukkit.api.PowerNukkitDifference;
+import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.Sinceb
 import cn.nukkit.entity.Entity;
 import cn.nukkit.event.entity.EntityDamageByChildEntityEvent;
 import cn.nukkit.event.entity.EntityDamageByEntityEvent;
