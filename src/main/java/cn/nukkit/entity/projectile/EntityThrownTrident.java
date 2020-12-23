acf3caedcd0f131b849cd1afb464c11f904a6aed
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-12-23 11:47:39 +0900

    fix

--- src/main/java/cn/nukkit/entity/projectile/EntityThrownTrident.java
+++ src/main/java/cn/nukkit/entity/projectile/EntityThrownTrident.java
@@ -372,7 +372,7 @@ public class EntityThrownTrident extends EntityProjectile {
     @Since("1.4.0.0-PN")
     public void setLoyaltyLevel(int loyaltyLevel) {
         this.loyaltyLevel = loyaltyLevel;
-        this.trident = this.trident.addEnchantment(Enchantment.get(Enchantment.ID_TRIDENT_LOYALTY).setLevel(loyaltyLevel));
+        this.trident = this.trident.clone().addEnchantment(Enchantment.get(Enchantment.ID_TRIDENT_LOYALTY).setLevel(loyaltyLevel));
     }
     
     @PowerNukkitOnly
