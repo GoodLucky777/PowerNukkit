e0ee614f52076952afe3faba05534ceffd66c908
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-12-27 19:34:18 +0900

    Fix import

--- src/main/java/cn/nukkit/item/enchantment/trident/EnchantmentTridentImpaling.java
+++ src/main/java/cn/nukkit/item/enchantment/trident/EnchantmentTridentImpaling.java
@@ -2,9 +2,11 @@ package cn.nukkit.item.enchantment.trident;
 
 import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.Since;
+import cn.nukkit.entity.Entity;
 import cn.nukkit.item.enchantment.Enchantment;
 
 public class EnchantmentTridentImpaling extends EnchantmentTrident {
+
     public EnchantmentTridentImpaling() {
         super(Enchantment.ID_TRIDENT_IMPALING, "impaling", 2);
     }
