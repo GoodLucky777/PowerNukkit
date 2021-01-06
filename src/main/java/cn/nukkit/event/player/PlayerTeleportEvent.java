715df7095de9d45f05a077ef4807b7fc5a9162bb
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-06 18:28:10 +0900

    Add END_PORTAL PlayerTeleportEvent TeleportCause

--- src/main/java/cn/nukkit/event/player/PlayerTeleportEvent.java
+++ src/main/java/cn/nukkit/event/player/PlayerTeleportEvent.java
@@ -62,6 +62,7 @@ public class PlayerTeleportEvent extends PlayerEvent implements Cancellable {
         NETHER_PORTAL, // Teleport using Nether portal
         ENDER_PEARL,   // Teleport by ender pearl
         CHORUS_FRUIT,  // Teleport by chorus fruit
-        UNKNOWN        // Unknown cause
+        UNKNOWN,       // Unknown cause
+        END_PORTAL     // Teleport using End portal
     }
 }
