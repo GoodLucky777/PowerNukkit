4619f50d5e921ceea7ad23f1a7b7cada9a5b977d
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-07 14:34:42 +0900

    Add generator

--- src/main/java/cn/nukkit/level/generator/TheEnd.java
+++ src/main/java/cn/nukkit/level/generator/TheEnd.java
@@ -1,25 +1,71 @@
 package cn.nukkit.level.generator;
 
-import cn.nukkit.block.*;
 import cn.nukkit.blockstate.BlockState;
 import cn.nukkit.level.ChunkManager;
 import cn.nukkit.level.Level;
 import cn.nukkit.level.biome.Biome;
 import cn.nukkit.level.biome.EnumBiome;
 import cn.nukkit.level.format.generic.BaseFullChunk;
+import cn.nukkit.level.generator.noise.vanilla.d.NoiseGeneratorOctavesD;
+import cn.nukkit.level.generator.noise.vanilla.d.NoiseGeneratorSimplexD;
 import cn.nukkit.level.generator.populator.type.Populator;
 import cn.nukkit.math.NukkitRandom;
 import cn.nukkit.math.Vector3;
 
-import java.util.*;
+import java.util.Random;
 
+/**
+ * @author GoodLucky777
+ * <p>
+ * This generator originally adapted from the Glowstone's TheEndGenerator (https://github.com/GlowstoneMC/Glowstone/blob/3de174426cd1e15e78050b9d691a3c1722c83181/src/main/java/net/glowstone/generator/TheEndGenerator.java)
+ * 
+ * Glowstone licensed under the following MIT license:
+ * 
+ * Glowstone Copyright (C) 2015-2020 The Glowstone Project.
+ * Glowstone Copyright (C) 2011-2014 Tad Hardesty.
+ * Lightstone Copyright (C) 2010-2011 Graham Edgecombe.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 public class TheEnd extends Generator {
 
     private ChunkManager level;
     private NukkitRandom nukkitRandom;
     private Random random;
     
+    private static final BlockState STATE_AIR = BlockState.of(AIR);
     private static final BlockState STATE_END_STONE = BlockState.of(END_STONE);
+    
+    private static double coordinateScale = 684.412;
+    private static double detailNoiseScaleX = 80;
+    private static double detailNoiseScaleZ = 80;
+    
+    private final NoiseGeneratorOctavesD lperlinNoise1;
+    private final NoiseGeneratorOctavesD lperlinNoise2;
+    private final NoiseGeneratorOctavesD perlinNoise1;
+    private final NoiseGeneratorSimplexD islandNoise;
+    
+    double[] pnr;
+    double[] ar;
+    double[] br;
+    
+    private final double[][][] density = new double[3][3][33];
     
     private final List<Populator> populators = new ArrayList<>();
     private List<Populator> generationPopulators = new ArrayList<>();
@@ -68,7 +114,11 @@ public class TheEnd extends Generator {
         this.nukkitRandom.setSeed(this.level.getSeed());
         this.localSeed1 = this.random.nextLong();
         this.localSeed2 = this.random.nextLong();
-
+        
+        this.lperlinNoise1 = new NoiseGeneratorOctavesD(random, 16);
+        this.lperlinNoise2 = new NoiseGeneratorOctavesD(random, 16);
+        this.perlinNoise1 = new NoiseGeneratorOctavesD(random, 8);
+        this.islandNoise = new NoiseGeneratorSimplexD(random);
     }
     
     @Override
@@ -86,7 +136,78 @@ public class TheEnd extends Generator {
             }
         }
         
-        // TODO
+        int densityX = chunkX << 1;
+        int densityZ = chunkZ << 1;
+        
+        this.pnr = this.perlinNoise1.generateNoiseOctaves(this.pnr, densityX * 2, 0, densityZ * 2, 3, 33, 3, (coordinateScale * 2) / detailNoiseScaleX, 4.277575000000001, (coordinateScale * 2) / detailNoiseScaleZ);
+        this.ar = this.lperlinNoise1.generateNoiseOctaves(this.ar, densityX * 2, 0, densityZ * 2, 3, 33, 3, coordinateScale * 2, coordinateScale, coordinateScale * 2);
+        this.br = this.lperlinNoise2.generateNoiseOctaves(this.br, densityX * 2, 0, densityZ * 2, 3, 33, 3, coordinateScale * 2, coordinateScale, coordinateScale * 2);
+        
+        int index = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                float noiseHeight = this.getIslandHeight(densityX, densityZ, i, j);
+                for (int k = 0; k < 33; k++) {
+                    double noiseR = this.ar[k] / 512;
+                    double noiseR2 = this.br[k] / 512;
+                    double noiseD = (this.pnr[k] / 10 + 1) / 2;
+                    // linear interpolation
+                    double dens = noiseD < 0 ? noiseR : noiseD > 1 ? noiseR2 : noiseR + (noiseR2 - noiseR) * noiseD;
+                    dens = (dens - 8) + noiseHeight;
+                    index++;
+                    if (k < 8) {
+                        double lowering = (double)((float)(k1 - k) / ((float)8 - 1));
+                        dens = dens * (1 - lowering) + -30 * lowering;
+                    } else if (k > (33 / 2) - 2) {
+                        double lowering = (double)((float)(k - ((33 / 2) - 2)) / 64);
+                        lowering = Math.clamp(lowering, 0, 1);
+                        dens = dens * (1 - lowering) + -3000 * lowering;
+                    }
+                    density[i][j][k] = dens;
+                }
+            }
+        }
+        
+        for (int i = 0; i < 3 - 1; i++) {
+            for (int j = 0; j < 3 - 1; j++) {
+                for (int k = 0; k < 33 - 1; k++) {
+                    double d1 = density[i][j][k];
+                    double d2 = density[i + 1][j][k];
+                    double d3 = density[i][j + 1][k];
+                    double d4 = density[i + 1][j + 1][k];
+                    double d5 = (density[i][j][k + 1] - d1) / 4;
+                    double d6 = (density[i + 1][j][k + 1] - d2) / 4;
+                    double d7 = (density[i][j + 1][k + 1] - d3) / 4;
+                    double d8 = (density[i + 1][j + 1][k + 1] - d4) / 4;
+                    
+                    for (int l = 0; l < 4; l++) {
+                        double d9 = d1;
+                        double d10 = d3;
+                        for (int m = 0; m < 8; m++) {
+                            double dens = d9;
+                            for (int n = 0; n < 8; n++) {
+                                // any density > 0 is ground, any density <= 0 is air.
+                                if (dens > 0) {
+                                    chunk.setBlockState(m + (i << 3), l + (k << 2), n + (j << 3), STATE_END_STONE);
+                                }
+                                // interpolation along z
+                                dens += (d10 - d9) / 8;
+                            }
+                            // interpolation along x
+                            d9 += (d2 - d1) / 8;
+                            // interpolate along z
+                            d10 += (d4 - d3) / 8;
+                        }
+                        // interpolation along y
+                        d1 += d5;
+                        d3 += d7;
+                        d2 += d6;
+                        d4 += d8;
+                    }
+                }
+            }
+        }
         
         for (Populator populator : this.generationPopulators) {
             populator.populate(this.level, chunkX, chunkZ, this.nukkitRandom, chunk);
@@ -107,5 +228,29 @@ public class TheEnd extends Generator {
     
     public Vector3 getSpawn() {
         return new Vector3(100.5, 49, 0.5);
+    }
+    
+    private float getIslandHeight(int densityX, int densityZ, int chunkX, int chunkZ) {
+        float x1 = (float) (densityX * 2 + chunkX);
+        float z1 = (float) (densityZ * 2 + chunkZ);
+        float islandHeight1 = Math.clamp(100 - Math.sqrt(Math.pow(x1, 2) + Math.pow(z1, 2)) * 8, -100, 80);
+        
+        for (int i = -12; i <= 12; i++) {
+            for (int j = -12; j <= 12; j++) {
+                long x2 = (long) (densityX + i);
+                long z2 = (long) (densityZ + j);
+                if (Math.pow(x2, 2) + Math.pow(z2, 2) > 4096L && this.islandNoise.getValue((double) x2, (double) z2) < -0.8999999761581421D) {
+                    x1 = (float) (chunkX - i * 2);
+                    z1 = (float) (chunkZ - j * 2);
+                    float islandHeight2 = Math.clamp(100 - Math.sqrt(Math.pow(x1, 2) + Math.pow(z1, 2)) * ((Math.abs((float) x2) * 3439 + Math.abs((float) z2) * 147) % 13 + 9)), -80, 100);
+                    
+                    if (islandHeight2 > islandHeight1) {
+                        islandHeight1 = islandHeight2;
+                    }
+                }
+            }
+        }
+        
+        return islandHeight1;
     }
 }
