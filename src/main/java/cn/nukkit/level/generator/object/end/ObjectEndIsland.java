d30747f57d9e51b9cbcc2ea5ab6a2ccc30557fbc
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-08 22:01:32 +0900

    Create ObjectEndIsland.java

---
+++ src/main/java/cn/nukkit/level/generator/object/end/ObjectEndIsland.java
@@ -0,0 +1,12 @@
+package cn.nukkit.level.generator.object.end;
+
+import cn.nukkit.level.ChunkManager;
+import cn.nukkit.math.NukkitRandom;
+import cn.nukkit.math.Vector3;
+
+public class ObjectEndIsland extends BasicGenerator {
+
+    public boolean generate(ChunkManager level, NukkitRandom rand, Vector3 position) {
+        
+    }
+}
