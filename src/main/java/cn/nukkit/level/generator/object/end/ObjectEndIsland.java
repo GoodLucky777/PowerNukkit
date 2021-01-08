75f56dc52fe9c062bb6ced42a52f70ea655eee70
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-08 22:03:15 +0900

    fix

--- src/main/java/cn/nukkit/level/generator/object/end/ObjectEndIsland.java
+++ src/main/java/cn/nukkit/level/generator/object/end/ObjectEndIsland.java
@@ -1,12 +1,13 @@
 package cn.nukkit.level.generator.object.end;
 
 import cn.nukkit.level.ChunkManager;
+import cn.nukkit.level.generator.object.BasicGenerator;
 import cn.nukkit.math.NukkitRandom;
 import cn.nukkit.math.Vector3;
 
 public class ObjectEndIsland extends BasicGenerator {
 
     public boolean generate(ChunkManager level, NukkitRandom rand, Vector3 position) {
-        
+        // TODO
     }
 }
