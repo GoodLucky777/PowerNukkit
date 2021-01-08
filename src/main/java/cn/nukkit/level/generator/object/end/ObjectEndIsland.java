ce6d952c2bcc415cc11ea090df7f2ad04fa75bed
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-08 22:19:51 +0900

    fix

--- src/main/java/cn/nukkit/level/generator/object/end/ObjectEndIsland.java
+++ src/main/java/cn/nukkit/level/generator/object/end/ObjectEndIsland.java
@@ -14,16 +14,16 @@ public class ObjectEndIsland extends BasicGenerator {
     private static final BlockState STATE_END_STONE = BlockState.of(END_STONE);
     
     public boolean generate(ChunkManager level, NukkitRandom rand, Vector3 position) {
-        float n = (float)(rand.nextBoundedInt(3) + 4);
+        float n = (float) (rand.nextBoundedInt(3) + 4);
         for(int y = 0; n > 0.5f; y--) {
             for(int x = Math.floor(-n); x <= Math.ceil(n); x++) {
                 for(int z = Math.floor(-m); z <= Math.ceil(n); z++) {
-                    if ((float)(x * x + z * z) <= (n + 1f) * (n + 1f)) {
+                    if ((float) (x * x + z * z) <= (n + 1f) * (n + 1f)) {
                         level.setBlockStateAt(position.getX() + x, position.getY() + y, position.getZ() + z, STATE_END_STONE);
                     }
                 }
             }
-            n -= (float)(rand.nextBoundedInt(2) + 0.5);
+            n -= (float) (rand.nextBoundedInt(2) + 0.5);
         }
         return true;
     }
