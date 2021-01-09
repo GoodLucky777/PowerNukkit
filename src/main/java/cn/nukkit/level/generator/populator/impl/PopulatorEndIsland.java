eb0d1a357dc6738f570468bca858f89f58432cd1
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-09 11:10:31 +0900

    improve

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndIsland.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndIsland.java
@@ -28,10 +28,12 @@ public class PopulatorEndIsland extends Populator {
             return;
         }
         
-        Vector3 position = new Vector3(chunkX << 4, 0, chunkZ << 4);
         if (random.nextBoundedInt(14) == 0) {
             float height = theEnd.getIslandHeight(chunkX, chunkZ, 1, 1);
+            
             if (height < -20f) {
+                Vector3 position = new Vector3(chunkX << 4, 0, chunkZ << 4);
+                
                 ObjectEndIsland.generate(level, random, position.add(8 + random.nextBoundedInt(16), 55 + random.nextBoundedInt(16), 8 + random.nextBoundedInt(16)));
                 if (random.nextBoundedInt(4) == 0) {
                     ObjectEndIsland.generate(level, random, position.add(8 + random.nextBoundedInt(16), 55 + random.nextBoundedInt(16), 8 + random.nextBoundedInt(16)));
