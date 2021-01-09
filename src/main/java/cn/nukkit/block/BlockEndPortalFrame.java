9fc37892f08207c3843dc6273d29cd4047e8cba5
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-09 15:09:29 +0900

    fix face

--- src/main/java/cn/nukkit/block/BlockEndPortalFrame.java
+++ src/main/java/cn/nukkit/block/BlockEndPortalFrame.java
@@ -236,7 +236,7 @@ public class BlockEndPortalFrame extends BlockTransparentMeta implements Faceabl
         if (player == null) {
             setBlockFace(BlockFace.SOUTH);
         } else {
-            setBlockFace(player.getDirection());
+            setBlockFace(player.getDirection().getOpposite());
         }
         this.getLevel().setBlock(block, this, true);
         return true;
