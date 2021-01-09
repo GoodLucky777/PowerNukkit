c8306a71317f2815f15a29170284676339c3793a
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-09 10:45:00 +0900

    fix

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndIsland.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndIsland.java
@@ -3,6 +3,7 @@ package cn.nukkit.level.generator.populator.impl;
 import cn.nukkit.blockstate.BlockState;
 import cn.nukkit.level.ChunkManager;
 import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.level.generator.TheEnd;
 import cn.nukkit.level.generator.populator.type.Populator;
 import cn.nukkit.math.NukkitRandom;
 
@@ -11,7 +12,13 @@ import cn.nukkit.math.NukkitRandom;
  */
 public class PopulatorEndIsland extends Populator {
 
+    private final TheEnd theEnd;
+    
     private static final BlockState STATE_END_STONE = BlockState.of(END_STONE);
+    
+    public PopulatorTheEnd(TheEnd theEnd) {
+        this.theEnd = theEnd;
+    }
     
     @Override
     public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
@@ -20,7 +27,7 @@ public class PopulatorEndIsland extends Populator {
         }
         
         if (random.nextBoundedInt(14) == 0) {
-            float height = getIslandHeight(chunkX, chunkZ, 1, 1);
+            float height = theEnd.getIslandHeight(chunkX, chunkZ, 1, 1);
             if (height < -20f) {
                 //
                 if (random.nextBoundedInt(4) == 0) {
