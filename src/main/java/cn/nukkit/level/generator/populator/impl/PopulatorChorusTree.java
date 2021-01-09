00ae01f4f1537e2ff7d2872b629c19b7957a8701
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-09 21:41:57 +0900

    Create PopulatorChorusTree.java

---
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorChorusTree.java
@@ -0,0 +1,37 @@
+package cn.nukkit.level.generator.populator.impl;
+
+import cn.nukkit.level.ChunkManager;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.level.generator.TheEnd;
+//import cn.nukkit.level.generator.object.end.ObjectChorus;
+import cn.nukkit.level.generator.populator.type.Populator;
+import cn.nukkit.math.NukkitRandom;
+import cn.nukkit.math.Vector3;
+
+/**
+ * @author GoodLucky777
+ */
+public class PopulatorChorusTree extends Populator {
+
+    private final TheEnd theEnd;
+    
+    //private final ObjectEndIsland objectEndIsland;
+    
+    public PopulatorChorusTree(TheEnd theEnd) {
+        this.theEnd = theEnd;
+        //this.objectChorus = new ObjectChorus();
+    }
+    
+    @Override
+    public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
+        if ((long) chunkX * (long) chunkX + (long) chunkZ * (long) chunkZ <= 4096L) {
+            return;
+        }
+        
+        if (theEnd.getIslandHeight(chunkX, chunkZ, 1, 1) > 40f) {
+            for (int i = 0; i < random.nextBoundedInt(5); i++) {
+                // TODO
+            }
+        }
+    }
+}
