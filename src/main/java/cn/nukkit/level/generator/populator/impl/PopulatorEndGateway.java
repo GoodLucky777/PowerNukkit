11bb17d0cdaf3e780136dbacf27787af32e09e15
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-10 22:27:40 +0900

    update

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndGateway.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndGateway.java
@@ -33,7 +33,13 @@ public class PopulatorEndGateway extends Populator {
         
         if (theEnd.getIslandHeight(chunkX, chunkZ, 1, 1) > 40f) {
             if (random.nextBoundedInt(700) == 0) {
-                // TODO
+                int x = (chunkX << 4) + random.nextBoundedInt(16) + 8;
+                int z = (chunkZ << 4) + random.nextBoundedInt(16) + 8;
+                int y = this.getHighestWorkableBlock(level, x, z, chunk);
+                
+                if (y > 0) {
+                    // TODO
+                }
             }
         }
     }
