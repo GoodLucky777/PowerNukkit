fb633220e63005609ad1db0058a0fa186216179b
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-10 17:02:12 +0900

    update

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorChorusTree.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorChorusTree.java
@@ -8,6 +8,8 @@ import cn.nukkit.level.generator.populator.type.Populator;
 import cn.nukkit.math.NukkitRandom;
 import cn.nukkit.math.Vector3;
 
+import static cn.nukkit.block.BlockID.END_STONE;
+
 /**
  * @author GoodLucky777
  */
@@ -16,6 +18,8 @@ public class PopulatorChorusTree extends Populator {
     private final TheEnd theEnd;
     
     private final ObjectChorusTree objectChorusTree;
+    
+    private static final BlockState STATE_END_STONE = BlockState.of(END_STONE);
     
     public PopulatorChorusTree(TheEnd theEnd) {
         this.theEnd = theEnd;
@@ -30,7 +34,14 @@ public class PopulatorChorusTree extends Populator {
         
         if (theEnd.getIslandHeight(chunkX, chunkZ, 1, 1) > 40f) {
             for (int i = 0; i < random.nextBoundedInt(5); i++) {
-                // TODO
+                int x = (chunkX << 4) + random.nextBoundedInt(16) + 8;
+                int z = (chunkZ << 4) + random.nextBoundedInt(16) + 8;
+                int y = this.getHighestWorkableBlock(level, x, z, chunk);
+                if (y > 0) {
+                    if (level.getBlockStateAt(x, y, z).equals(BlockState.AIR) && level.getBlockStateAt(x, y - 1, z).equals(STATE_END_STONE)) {
+                        objectChorusTree.generate(level, random, new Vector3(x, y, z), 8);
+                    }
+                }
             }
         }
     }
