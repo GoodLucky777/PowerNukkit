31f674bc26267099402534e005534ffc93d05e8b
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-11 21:50:05 +0900

    improve

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndGateway.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndGateway.java
@@ -41,10 +41,10 @@ public class PopulatorEndGateway extends Populator {
                 
                 if (y > 1 && y < 254) {
                     if (objectEndGateway.generate(level, random, new Vector3(x, y, z))) {
-                        /*BlockEntity blockEntity = chunk.getTile(x & 0x0f, y & 0xff, z & 0x0f);
-                        if (blockEntity != null) {
+                        BlockEntity blockEntity = chunk.getTile(x & 0x0f, y & 0xff, z & 0x0f);
+                        if (blockEntity != null && blockEntity instanceof BlockEntityEndGateway) {
                             ((BlockEntityEndGateway) blockEntity).setExitPortal(theEnd.getSpawn().asBlockVector3());
-                        }*/
+                        }
                     }
                 }
             }
