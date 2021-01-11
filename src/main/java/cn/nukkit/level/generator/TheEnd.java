4e15741bfdf2e0d38cd2e32cfd8a05ef72e430d7
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-11 14:06:24 +0900

    Add PopulatorEndGateway to TheEnd generator

--- src/main/java/cn/nukkit/level/generator/TheEnd.java
+++ src/main/java/cn/nukkit/level/generator/TheEnd.java
@@ -9,6 +9,7 @@ import cn.nukkit.level.format.generic.BaseFullChunk;
 import cn.nukkit.level.generator.noise.vanilla.d.NoiseGeneratorOctavesD;
 import cn.nukkit.level.generator.noise.vanilla.d.NoiseGeneratorSimplexD;
 import cn.nukkit.level.generator.populator.impl.PopulatorChorusTree;
+import cn.nukkit.level.generator.populator.impl.PopulatorEndGateway;
 import cn.nukkit.level.generator.populator.impl.PopulatorEndIsland;
 import cn.nukkit.level.generator.populator.type.Populator;
 import cn.nukkit.math.MathHelper;
@@ -127,7 +128,8 @@ public class TheEnd extends Generator {
         
         this.populators = ImmutableList.of(
             new PopulatorEndIsland(this),
-            new PopulatorChorusTree(this)
+            new PopulatorChorusTree(this),
+            new PopulatorEndGateway(this)
         );
     }
     
