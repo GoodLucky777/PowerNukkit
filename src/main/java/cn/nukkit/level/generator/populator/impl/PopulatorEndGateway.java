4f5ea04b296fa86445cb5aa05ebf14f8f118193e
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-11 14:52:23 +0900

    fix

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndGateway.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndGateway.java
@@ -37,10 +37,9 @@ public class PopulatorEndGateway extends Populator {
             if (random.nextBoundedInt(700) == 0) {
                 int x = (chunkX << 4) + random.nextBoundedInt(16) + 8;
                 int z = (chunkZ << 4) + random.nextBoundedInt(16) + 8;
-                int y = this.getHighestWorkableBlock(level, x, z, chunk);
+                int y = this.getHighestWorkableBlock(level, x, z, chunk) + random.nextBoundedInt(7) + 3;
                 
                 if (y > 0 && y < 254) {
-                    y += 3 + random.nextBoundedInt(7);
                     objectEndGateway.generate(level, random, new Vector3(x, y, z));
                     BlockEntity blockEntity = chunk.getTile(x & 0x0f, y & 0xff, z & 0x0f);
                     if (blockEntity != null) {
