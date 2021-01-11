880381c20460b9276423ee752c9b35599cbe12b0
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-11 10:08:10 +0900

    update

--- src/main/java/cn/nukkit/level/generator/object/end/ObjectEndGateway.java
+++ src/main/java/cn/nukkit/level/generator/object/end/ObjectEndGateway.java
@@ -8,6 +8,7 @@ import cn.nukkit.math.NukkitRandom;
 import cn.nukkit.math.Vector3;
 
 import static cn.nukkit.block.BlockID.BEDROCK;
+import static cn.nukkit.block.BlockID.END_GATEWAY;
 
 /**
  * @author GoodLucky777
@@ -15,9 +16,31 @@ import static cn.nukkit.block.BlockID.BEDROCK;
 public class ObjectEndGateway extends BasicGenerator {
 
     private static final BlockState STATE_BEDROCK = BlockState.of(BEDROCK);
+    private static final BlockState STATE_END_GATEWAY = BlockState.of(END_GATEWAY);
     
     public boolean generate(ChunkManager level, NukkitRandom rand, Vector3 position) {
-        // TODO
+        for (int x = position.getFloorX() - 1; x <= position.getFloorX() + 1; x++) {
+            for (int z = position.getFloorZ() - 1; x <= position.getFloorZ() + 1; z++) {
+                for (int y = position.getFloorY() - 2; y <= position.getFloorY() + 2; y++) {
+                    boolean flagX = position.getFloorX() == x;
+                    boolean flagY = position.getFloorY() == y;
+                    boolean flagZ = position.getFloorZ() == z;
+                    boolean flagFar = Math.abs(y - position.getFloorY()) == 2;
+                    
+                    if (flagX && flagY && flagZ) {
+                        level.setBlockStateAt(x, y, z, STATE_END_GATEWAY);
+                    } else if (flagX && flagZ && flagFar) {
+                        level.setBlockStateAt(x, y, z, STATE_BEDROCK);
+                    } else if (!flagFar && (flagX || flagZ)) {
+                        level.setBlockStateAt(x, y, z, STATE_BEDROCK);
+                    } else if (flagY) {
+                        level.setBlockStateAt(x, y, z, BlockState.AIR);
+                    } else {
+                        level.setBlockStateAt(x, y, z, BlockState.AIR);
+                    }
+                }
+            }
+        }
         return true;
     }
 }
