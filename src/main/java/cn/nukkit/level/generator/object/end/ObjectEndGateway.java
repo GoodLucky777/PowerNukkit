7a8dc71d4ba8caa48776f6f39936e9f0433cca1d
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-13 16:53:43 +0900

    update

--- src/main/java/cn/nukkit/level/generator/object/end/ObjectEndGateway.java
+++ src/main/java/cn/nukkit/level/generator/object/end/ObjectEndGateway.java
@@ -1,10 +1,13 @@
 package cn.nukkit.level.generator.object.end;
 
+import cn.nukkit.blockentity.BlockEntity;
+import cn.nukkit.blockentity.BlockEntiyEndGateway;
 import cn.nukkit.blockstate.BlockState;
 import cn.nukkit.level.ChunkManager;
 import cn.nukkit.level.generator.object.BasicGenerator;
 import cn.nukkit.math.NukkitMath;
 import cn.nukkit.math.NukkitRandom;
+import cn.nukkit.math.BlockVector3;
 import cn.nukkit.math.Vector3;
 
 import static cn.nukkit.block.BlockID.BEDROCK;
@@ -18,7 +21,7 @@ public class ObjectEndGateway extends BasicGenerator {
     private static final BlockState STATE_BEDROCK = BlockState.of(BEDROCK);
     private static final BlockState STATE_END_GATEWAY = BlockState.of(END_GATEWAY);
     
-    public boolean generate(ChunkManager level, NukkitRandom rand, Vector3 position) {
+    public boolean generate(ChunkManager level, NukkitRandom rand, Vector3 position, BlockVector3 exitPortal) {
         for (int x = position.getFloorX() - 1; x <= position.getFloorX() + 1; x++) {
             for (int z = position.getFloorZ() - 1; z <= position.getFloorZ() + 1; z++) {
                 for (int y = position.getFloorY() - 2; y <= position.getFloorY() + 2; y++) {
@@ -29,6 +32,7 @@ public class ObjectEndGateway extends BasicGenerator {
                     
                     if (flagX && flagY && flagZ) {
                         level.setBlockStateAt(x, y, z, STATE_END_GATEWAY);
+                        BlockEntityEndGateway endGateway = new BlockEntity.createBlockEntity("EndGateway", level.getChunk(x >> 4, z >> 4), BlockEntity.getDefaultCompound());
                     } else if (flagY) {
                         level.setBlockStateAt(x, y, z, BlockState.AIR);
                     } else if (flagX && flagZ && flagFar) {
