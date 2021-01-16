a498dcbee8ba29bcb730541c091cf0507cadf338
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-16 15:46:58 +0900

    update

--- src/main/java/cn/nukkit/level/generator/structure/Structure.java
+++ src/main/java/cn/nukkit/level/generator/structure/Structure.java
@@ -1,10 +1,22 @@
 package cn.nukkit.level.generator.structure.Structure;
 
+import cn.nukkit.math.BlockVector3;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.IntTag;
+import cn.nukkit.nbt.tag.ListTag;
 
 /**
  * @author GoodLucky777
  */
 public class Structure {
 
+    private BlockVector3 size = new BlockVector3(0, 0, 0);
     
+    public void loadStructure(CompoundTag compoundTag) {
+        // Load size
+        ListTag<IntTag> sizeList = compoundTag.getList("size", IntTag.class);
+        this.size = new BlockVector3(sizeList.get(0).data, sizeList.get(1).data, sizeList.get(2).data);
+        
+        
+    }
 }
