944a75c4bdb54253968ff108b3b739db8c7e2f68
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-17 11:10:31 +0900

    update

--- src/main/java/cn/nukkit/level/generator/structure/StructureTemplate.java
+++ src/main/java/cn/nukkit/level/generator/structure/StructureTemplate.java
@@ -1,10 +1,13 @@
 package cn.nukkit.level.generator.structure.Structure;
 
 import cn.nukkit.math.BlockVector3;
+import cn.nukkit.math.Vector3;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.nbt.tag.DoubleTag;
 import cn.nukkit.nbt.tag.IntTag;
 import cn.nukkit.nbt.tag.ListTag;
+
+import java.util.List;
 
 /**
  * @author GoodLucky777
@@ -12,6 +15,7 @@ import cn.nukkit.nbt.tag.ListTag;
 public class StructureTemplate {
 
     private BlockVector3 size = new BlockVector3(0, 0, 0);
+    private List<StructureTemplate.EntityEntry> entities = new List<StructureTemplate.EntityEntry>();
     
     public void loadStructure(CompoundTag compoundTag) {
         // Load size
@@ -24,13 +28,29 @@ public class StructureTemplate {
         
         // Load entities
         ListTag<CompoundTag> entitiesList = compoundTag.getList("entities", CompoundTag.class);
-        for (int i = 0; entitiesList.size(); i++) {
+        for (int i = 0; i < entitiesList.size(); i++) {
             CompoundTag entityTag = entitiesList.get(i);
             if (entityTag.contains("pos") && entityTag.contains("blockPos") && entityTag.contains("nbt")) {
-                ListTag<DoubleTag> pos = entityTag.getList("pos", DoubleTag.class);
-                ListTag<IntTag> blockPos = entityTag.getList("blockPos", IntTag.class);
-                
+                ListTag<DoubleTag> posTag = entityTag.getList("pos", DoubleTag.class);
+                Vector3 pos = new Vector3(posTag.get(0).data, posTag.get(1).data, posTag.get(2).data);
+                ListTag<IntTag> blockPosTag = entityTag.getList("blockPos", IntTag.class);
+                BlockVector3 blockPos = new BlockVector3(blockPosTag.get(0).data, blockPosTag.get(1).data, blockPosTag.get(2).data);
+                CompoundTag nbt = entityTag.getCompound("nbt");
+                this.entities.add(new StructureTemplate.EntityEntry(pos, blockPos, nbt));
             }
+        }
+    }
+    
+    private static class EntityEntry {
+    
+        private final Vector3 pos;
+        private final BlockVector3 blockPos;
+        private final CompoundTag nbt;
+        
+        public EntityEntry(Vector3 pos, BlockVector3 blockPos, CompoundTag nbt) {
+            this.pos = pos;
+            this.blockPos = blockPos;
+            this.nbt = nbt;
         }
     }
 }
