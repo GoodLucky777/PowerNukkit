c509038f23404d35673f17267fa5362626504883
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-02-05 14:08:40 +0900

    update

--- src/main/java/cn/nukkit/network/protocol/ItemComponentPacket.java
+++ src/main/java/cn/nukkit/network/protocol/ItemComponentPacket.java
@@ -1,7 +1,12 @@
 package cn.nukkit.network.protocol;
 
+import cn.nukkit.nbt.tag.CompoundTag;
+
 import lombok.ToString;
 
+/**
+ * @author GoodLucky777
+ */
 @ToString
 public class ItemComponentPacket extends DataPacket {
 
@@ -9,15 +14,35 @@ public class ItemComponentPacket extends DataPacket {
     public byte pid() {
         return ProtocolInfo.ITEM_COMPONENT_PACKET;
     }
-
+    
     @Override
     public void decode() {
         
     }
-
+    
     @Override
     public void encode() {
         this.reset();
         this.putVarInt(0); // TODO
+    }
+    
+    @ToString
+    public static class Entry {
+        public static final Entry[] EMPTY_ARRAY = new Entry[0];
+        
+        public final String name;
+        public final CompoundTag data;
+        
+        public Entry(String name, CompoundTag data) {
+            this(name, data);
+        }
+        
+        public String getName() {
+            return name;
+        }
+        
+        public CompoundTag getData() {
+            return data;
+        }
     }
 }
