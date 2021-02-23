4f4b52c5257c2909d91de990d95989072d6ee40c
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-02-23 12:36:07 +0900

    fix

--- src/main/java/cn/nukkit/blockentity/BlockEntityCommandBlock.java
+++ src/main/java/cn/nukkit/blockentity/BlockEntityCommandBlock.java
@@ -1,6 +1,7 @@
 package cn.nukkit.blockentity;
 
 import cn.nukkit.Player;
+import cn.nukkit.Server;
 import cn.nukkit.block.Block;
 import cn.nukkit.block.BlockCommand;
 import cn.nukkit.block.BlockID;
