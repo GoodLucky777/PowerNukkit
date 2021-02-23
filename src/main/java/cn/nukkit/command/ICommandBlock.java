511dfdcdcb351a1fadb9e7a249ac4ace259a8909
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-02-23 10:14:52 +0900

    update

--- src/main/java/cn/nukkit/command/ICommandBlock.java
+++ src/main/java/cn/nukkit/command/ICommandBlock.java
@@ -8,6 +8,7 @@ import cn.nukkit.permission.PermissibleBase;
 import cn.nukkit.permission.Permission;
 import cn.nukkit.permission.PermissionAttachment;
 import cn.nukkit.permission.PermissionAttachmentInfo;
+import cn.nukkit.plugin.Plugin;
 
 import java.util.Map;
 
@@ -17,14 +18,46 @@ public interface ICommandBlock extends CommandSender {
     
     PermissibleBase perm = new PermissibleBase(this);
     
+    static PermissionAttachment addAttachment(Plugin plugin) {
+        return this.perm.addAttachment(plugin);
+    }
+    
+    static PermissionAttachment addAttachment(Plugin plugin, String name) {
+        return this.perm.addAttachment(plugin, name);
+    }
+    
+    static PermissionAttachment addAttachment(Plugin plugin, String name, Boolean value) {
+        return this.perm.addAttachment(plugin, name, value);
+    }
+    
     static int getCurrentVersion() {
         return CURRENT_VERSION;
     }
     
+    static Map<String, PermissionAttachmentInfo> getEffectivePermissions() {
+        return this.perm.getEffectivePermissions();
+    }
+    
+    default String getName() {
+        return "Command Block";
+    }
+    
+    static Server getServer() {
+        return Server.getInstance();
+    }
+    
     int getVersion();
+    
+    static boolean hasPermission(Permission permission) {
+        return this.perm.hasPermission(permission);
+    }
     
     static boolean hasPermission(String name) {
         return this.perm.hasPermission(name);
+    }
+    
+    static boolean isOp() {
+        return true;
     }
     
     static boolean isPermissionSet(Permission permission) {
@@ -33,6 +66,38 @@ public interface ICommandBlock extends CommandSender {
     
     static boolean isPermissionSet(String name) {
         return this.perm.isPermissionSet(name);
+    }
+    
+    static boolean isPlayer() {
+        return false;
+    }
+    
+    static void recalculatePermissions() {
+        this.perm.recalculatePermissions();
+    }
+    
+    static void removeAttachment(PermissionAttachment attachment) {
+        this.perm.removeAttachment(attachment);
+    }
+    
+    static void sendMessage(String message) {
+        message = this.getServer().getLanguage().translateString(message);
+        
+        if (Server.getInstance().getDefaultLevel().getGameRules().getBoolean(GameRule.COMMAND_BLOCK_OUTPUT)) {
+            for (Player player : Server.getInstance().getDefaultLevel().getPlayers().values()) {
+                if (player.isOp()) {
+                    player.sendMessage(message);
+                }
+            }
+        }
+    }
+    
+    static void sendMessage(TextContainer message) {
+        this.sendMessage(this.getServer().getLanguage().translate(message));
+    }
+    
+    static void setOp(boolean value) {
+        // Does Nothing
     }
     
     void setVersion(int version);
