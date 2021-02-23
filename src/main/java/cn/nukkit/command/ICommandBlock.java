e8349a58409335f33bd93f74d8375f3865d40562
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-02-23 10:02:35 +0900

    update

--- src/main/java/cn/nukkit/command/ICommandBlock.java
+++ src/main/java/cn/nukkit/command/ICommandBlock.java
@@ -1,14 +1,43 @@
 package cn.nukkit.command;
 
-public interface ICommandBlock {
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.lang.TextContainer;
+import cn.nukkit.level.GameRule;
+import cn.nukkit.permission.PermissibleBase;
+import cn.nukkit.permission.Permission;
+import cn.nukkit.permission.PermissionAttachment;
+import cn.nukkit.permission.PermissionAttachmentInfo;
+
+import java.util.Map;
+
+public interface ICommandBlock extends CommandSender {
 
     public int CURRENT_VERSION = 13;
+    
+    private PermissibleBase perm;
+    
+    static ICommandBlock {
+        this.perm = new PermissibleBase(this);
+    }
     
     static int getCurrentVersion() {
         return CURRENT_VERSION;
     }
     
     int getVersion();
+    
+    static boolean hasPermission(String name) {
+        return this.perm.hasPermission(name);
+    }
+    
+    static boolean isPermissionSet(Permission permission) {
+        return this.perm.isPermissionSet(permission);
+    }
+    
+    static boolean isPermissionSet(String name) {
+        return this.perm.isPermissionSet(name);
+    }
     
     void setVersion(int version);
 }
