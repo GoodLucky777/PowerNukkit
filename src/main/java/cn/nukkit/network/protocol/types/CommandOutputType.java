5245b58234300e4f8d71e064ee164b073e4f77b6
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-02-24 14:47:34 +0900

    Create CommandOutputType.java

---
+++ src/main/java/cn/nukkit/network/protocol/types/CommandOutputType.java
@@ -0,0 +1,12 @@
+package cn.nukkit.network.protocol.types;
+
+/**
+ * @author GoodLucky777
+ */
+public enum CommandOutputType {
+    NONE,
+    LAST_OUTPUT,
+    SILENT,
+    ALL_OUTPUT,
+    DATA_SET
+}
