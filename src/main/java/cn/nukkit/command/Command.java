3664888a0ef519baa00d2e85bcb2e7270eb4477f
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-02-25 12:33:35 +0900

    fix

--- src/main/java/cn/nukkit/command/Command.java
+++ src/main/java/cn/nukkit/command/Command.java
@@ -351,14 +351,8 @@ public abstract class Command {
     
     @PowerNukkitOnly
     @Since("1.4.0.0-PN")
-    public static void broadcastCommandOutput(CommandSender source, CommandOutputType type, int successCount, CommandOutputMessage message, String data) {
-        broadcastCommandOutput(source, type, successCount, new CommandOutputMessage[]{ message }, data);
-    }
-    
-    @PowerNukkitOnly
-    @Since("1.4.0.0-PN")
-    public static void broadcastCommandOutput(CommandSender source, CommandOutputType type, int successCount, CommandOutputMessage[] messages, String data) {
-        broadcastCommandOutput(source.getCommandOriginData(), type, successCount, messages, data);
+    public static void broadcastCommandOutput(CommandOriginData origin, CommandOutputType type, int successCount, CommandOutputMessage message, String data) {
+        broadcastCommandOutput(origin, type, successCount, new CommandOutputMessage[]{ message }, data);
     }
     
     @PowerNukkitOnly
