c5d2d8d685209f62b5c29ba35e0fb7905b73f458
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-02-25 10:12:19 +0900

    update weather command output

--- src/main/java/cn/nukkit/command/defaults/WeatherCommand.java
+++ src/main/java/cn/nukkit/command/defaults/WeatherCommand.java
@@ -8,6 +8,8 @@ import cn.nukkit.command.data.CommandParamType;
 import cn.nukkit.command.data.CommandParameter;
 import cn.nukkit.lang.TranslationContainer;
 import cn.nukkit.level.Level;
+import cn.nukkit.network.protocol.types.CommandOutputMessage;
+import cn.nukkit.network.protocol.types.CommandOutputType;
 
 /**
  * @author Angelic47 (Nukkit Project)
@@ -60,21 +62,18 @@ public class WeatherCommand extends VanillaCommand {
                 level.setThundering(false);
                 level.setRainTime(seconds * 20);
                 level.setThunderTime(seconds * 20);
-                Command.broadcastCommandMessage(sender,
-                        new TranslationContainer("commands.weather.clear"));
+                Command.broadcastCommandOutput(sender, CommandOutputType.ALL_OUTPUT, 1, new CommandOutputMessage(true, "commands.weather.clear"), null);
                 return true;
             case "rain":
                 level.setRaining(true);
                 level.setRainTime(seconds * 20);
-                Command.broadcastCommandMessage(sender,
-                        new TranslationContainer("commands.weather.rain"));
+                Command.broadcastCommandOutput(sender, CommandOutputType.ALL_OUTPUT, 1, new CommandOutputMessage(true, "commands.weather.rain"), null);
                 return true;
             case "thunder":
                 level.setThundering(true);
                 level.setRainTime(seconds * 20);
                 level.setThunderTime(seconds * 20);
-                Command.broadcastCommandMessage(sender,
-                        new TranslationContainer("commands.weather.thunder"));
+                Command.broadcastCommandOutput(sender, CommandOutputType.ALL_OUTPUT, 1, new CommandOutputMessage(true, "commands.weather.thunder"), null);
                 return true;
             default:
                 sender.sendMessage(new TranslationContainer("commands.weather.usage", this.usageMessage));
