d101ec4803ddfcae44ef34c7af7f110e8dca4834
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-02-26 13:44:55 +0900

    update

--- src/main/java/cn/nukkit/command/ICommandBlock.java
+++ src/main/java/cn/nukkit/command/ICommandBlock.java
@@ -1,5 +1,8 @@
 package cn.nukkit.command;
 
+import cn.nukkit.nbt.tag.ListTag;
+import cn.nukkit.nbt.tag.StringTag;
+
 public interface ICommandBlock extends CommandSender {
 
     public int CURRENT_VERSION = 13;
@@ -8,9 +11,58 @@ public interface ICommandBlock extends CommandSender {
         return CURRENT_VERSION;
     }
     
+    String getCommand();
+    
+    boolean getExecuteOnFirstTick();
+    
+    long getLastExecution();
+    
+    String getLastOutput();
+    
+    String[] getLastOutputParams();
+    
+    default ListTag<StringTag> getLastOutputParamsAsListTag() {
+        ListTag<StringTag> tempLastOutputParmas = new ListTag<StringTag>("LastOutputParams");
+        for (int i = 0; i < this.lastOutputParams.length; i++) {
+            tempLastOutputParmas.add(new StringTag("", this.lastOutputParams[i]));
+        }
+        
+        return tempLastOutputParmas;
+    }
+    
+    int getSuccessCount();
+    
+    int getTickDelay();
+    
+    boolean getTrackOutput();
+    
     int getVersion();
     
+    void reset();
+    
+    void setCommand(String command);
+    
+    void setExecuteOnFirstTick(boolean executeOnFirstTick);
+    
+    void setLastExecution(long lastExecution);
+    
+    void setLastOutput(String lastOutput);
+    
+    void setLastOutputParams(String lastOutputParams, int index);
+    
+    void setLastOutputParams(String[] lastOutputParams);
+    
+    void setSuccessCount(int successCount);
+    
+    void setTickDelay(int tickDelay);
+    
+    void setTrackOutput(boolean trackOutput);
+    
     void setVersion(int version);
+    
+    default boolean trigger() {
+        return this.trigger(1);
+    }
     
     boolean trigger(int successCount);
 }
