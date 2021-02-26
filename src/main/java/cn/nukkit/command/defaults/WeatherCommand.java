038057f9bea5c101d5616fcefbbf7492c067bac2
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-02-26 13:14:19 +0900

    fix return

--- src/main/java/cn/nukkit/command/defaults/WeatherCommand.java
+++ src/main/java/cn/nukkit/command/defaults/WeatherCommand.java
@@ -31,7 +31,7 @@ public class WeatherCommand extends VanillaCommand {
     
     @Override
     public boolean execute(CommandSender sender, String commandLabel, String[] args) {
-        this.execute(sender, commandLabel, args, CommandOriginData.DEFAULT);
+        return this.execute(sender, commandLabel, args, CommandOriginData.DEFAULT);
     }
     
     @PowerNukkitOnly
