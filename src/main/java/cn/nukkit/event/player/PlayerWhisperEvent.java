945937bdd73ae836e9f4712ff165736f7370d443
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-02-26 14:05:39 +0900

    Create PlayerWhisperEvent.java

---
+++ src/main/java/cn/nukkit/event/player/PlayerWhisperEvent.java
@@ -0,0 +1,41 @@
+package cn.nukkit.event.player;
+
+import cn.nukkit.Player;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.event.Cancellable;
+import cn.nukkit.event.HandlerList;
+import cn.nukkit.permission.Permissible;
+
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author GoodLucky777
+ */
+public class PlayerWhisperEvent extends PlayerMessageEvent implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+    
+    public static HandlerList getHandlers() {
+        return handlers;
+    }
+    
+    public PlayerChatEvent(@Nonnull Player player, @Nonnull String message, @Nonnull Set<CommandSender> recipients) {
+        this.player = player;
+        this.message = message;
+        this.recipients = recipients;
+    }
+    
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+    
+    public Set<CommandSender> getRecipients() {
+        return this.recipients;
+    }
+    
+    public void setRecipients(Set<CommandSender> recipients) {
+        this.recipients = recipients;
+    }
+}
