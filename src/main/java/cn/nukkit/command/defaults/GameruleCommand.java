f78b4e40776280470368cc58cd74a12fd0dc5e77
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-02-26 11:16:26 +0900

    fix

--- src/main/java/cn/nukkit/command/defaults/GameruleCommand.java
+++ src/main/java/cn/nukkit/command/defaults/GameruleCommand.java
@@ -80,7 +80,7 @@ public class GameruleCommand extends VanillaCommand {
     
     @Override
     public boolean execute(CommandSender sender, String commandLabel, String[] args) {
-        this.execute(sender, commandLine, args, CommandOriginData.DEFAULT);
+        this.execute(sender, commandLabel, args, CommandOriginData.DEFAULT);
     }
     
     @PowerNukkitOnly
