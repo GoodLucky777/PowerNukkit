e6d1888e4b933f0baab615ff901e68fb96173cf5
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-06 11:10:08 +0900

    fix

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -3235,10 +3235,6 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                                 this.level.dropItem(vector3, itemDrop);
                                 itemFrame.setItem(new ItemBlock(Block.get(BlockID.AIR)));
                                 itemFrame.setItemRotation(0);
-                                if (itemDrop.getId() == Item.MAP) {
-                                    ((BlockItemFrame) block).setItemFrameMap(false);
-                                    this.level.setBlock(block, block, true, true);
-                                }
                                 this.getLevel().addLevelEvent(this, LevelEventPacket.EVENT_SOUND_ITEM_FRAME_ITEM_REMOVED);
                             }
                         } else {
