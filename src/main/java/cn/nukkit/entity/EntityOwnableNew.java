7907da0d53237fc869d1e1c2f8f72d964c118bb5
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-07 21:25:57 +0900

    Update EntityOwnableNew.java

--- src/main/java/cn/nukkit/entity/EntityOwnableNew.java
+++ src/main/java/cn/nukkit/entity/EntityOwnableNew.java
@@ -1,11 +1,19 @@
 package cn.nukkit.entity;
 
+import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.Since;
+import cn.nukkit.entity.data.LongEntityData;
+
 /**
  * @author GoodLucky777
  */
+@PowerNukkitOnly
+@Since("1.4.0.0-PN")
 public interface EntityOwnableNew {
 
     long getOwnerId();
 
-    void setOwnerId(long ownerId);
+    default void setOwnerId(long ownerId) {
+        this.setDataProperty(new LongEntityData(Entity.DATA_OWNER_EID, ownerId));
+    }
 }
