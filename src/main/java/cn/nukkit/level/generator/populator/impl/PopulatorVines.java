d00ae37beb83664288991b9e597ab780e77889f0
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-09 21:00:04 +0900

    fix

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorVines.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorVines.java
@@ -1,10 +1,14 @@
 package cn.nukkit.level.generator.populator.impl;
 
+import cn.nukkit.block.BlockVine;
 import cn.nukkit.level.ChunkManager;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.level.generator.populator.helper.PopulatorHelpers;
 import cn.nukkit.level.generator.populator.type.PopulatorCount;
+import cn.nukkit.math.BlockFace;
 import cn.nukkit.math.NukkitRandom;
+
+import java.util.Set;
 
 /**
  * @author GoodLucky777
@@ -26,9 +30,25 @@ public class PopulatorVines extends PopulatorCount {
     @Override
     protected void placeBlock(int x, int y, int z, int id, FullChunk chunk, NukkitRandom random) {
         if (chunk.getBlockId(x, y, z) == AIR) {
+            Set<BlockFace> attachFaces = new Set<BlockFace>;
+            
             if (chunk.getBlockState(x - 1, y, z).getBlock().isSolid()) {
-                chunk.setBlockState(VINE, );
+                attachFaces.add(BlockFace.WEST);
             }
+            
+            if (chunk.getBlockState(x + 1, y, z).getBlock().isSolid()) {
+                attachFaces.add(BlockFace.EAST);
+            }
+            
+            if (chunk.getBlockState(x, y, z - 1).getBlock().isSolid()) {
+                attachFaces.add(BlockFace.SOUTH);
+            }
+            
+            if (chunk.getBlockState(x, y, z + 1).getBlock().isSolid()) {
+                attachFaces.add(BlockFace.NORTH);
+            }
+            
+            chunk.setBlockState(BlockState.of(VINE, BlockVine.getMetaFromFaces(attachFaces)));
         }
     }
 }
