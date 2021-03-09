d23e3b338c7ad2b0af8cd1ddeb0a13bfcf8a4ea3
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-09 13:12:55 +0900

    Create BambooJungleBiome.java

---
+++ src/main/java/cn/nukkit/level/biome/impl/jungle/BambooJungleBiome.java
@@ -0,0 +1,16 @@
+package cn.nukkit.level.biome.impl.jungle;
+
+/**
+ * @author GoodLucky777
+ */
+public class BambooJungleBiome extends JungleBiome {
+
+    public BambooJungleBiome() {
+        // TODO
+    }
+    
+    @Override
+    public String getName() {
+        return "Bamboo Jungle";
+    }
+}
