ab9938d382abc365bd94463f4ff1459f1711d46d
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-09 19:46:44 +0900

    update

--- src/main/java/cn/nukkit/level/biome/impl/jungle/BambooJungleBiome.java
+++ src/main/java/cn/nukkit/level/biome/impl/jungle/BambooJungleBiome.java
@@ -1,12 +1,43 @@
 package cn.nukkit.level.biome.impl.jungle;
 
+import cn.nukkit.level.generator.populator.impl.PopulatorBamboo;
+import cn.nukkit.level.generator.populator.impl.PopulatorMelon;
+import cn.nukkit.level.generator.populator.impl.PopulatorPumpkin;
+import cn.nukkit.level.generator.populator.impl.PopulatorTree;
+import cn.nukkit.level.generator.populator.impl.tree.JungleBigTreePopulator;
+
 /**
  * @author GoodLucky777
  */
 public class BambooJungleBiome extends JungleBiome {
 
     public BambooJungleBiome() {
-        // TODO
+        PopulatorBamboo bamboo = new PopulatorBamboo();
+        bamboo.setBaseAmount(20);
+        bamboo.setRandomAmount(15);
+        this.addPopulator(bamboo);
+        
+        // TODO: Vine Populator
+        
+        JungleBigTreePopulator jungleBigTrees = new JungleBigTreePopulator();
+        jungleBigTrees.setBaseAmount(2);
+        jungleBigTrees.setRandomAmount(1);
+        this.addPopulator(jungleBigTrees);
+        
+        PopulatorTree oakBigTree = new PopulatorTree(BlockSapling.OAK); // TODO: Should be only Fancy (Big) Oak Tree
+        oakBigTree.setBaseAmount(-1);
+        oakBigTree.setRandomAmount(2);
+        this.addPopulator(oakBigTree);
+        
+        PopulatorMelon melon = new PopulatorMelon();
+        melon.setBaseAmount(-65);
+        melon.setRandomAmount(70);
+        this.addPopulator(melon);
+        
+        PopulatorPumpkin pumpkin = new PopulatorPumpkin();
+        pumpkin.setBaseAmount(-65);
+        pumpkin.setRandomAmount(70);
+        this.addPopulator(pumpkin);
     }
     
     @Override
