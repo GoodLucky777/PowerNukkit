a1e14e4ea4978c12eb8ad228ef2c52d5ff11808c
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-09 20:42:36 +0900

    update

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorVines.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorVines.java
@@ -1,26 +1,26 @@
 package cn.nukkit.level.generator.populator.impl;
 
-import cn.nukkit.block.Block;
+import cn.nukkit.level.ChunkManager;
 import cn.nukkit.level.format.FullChunk;
-import cn.nukkit.level.generator.populator.helper.EnsureBelow;
-import cn.nukkit.level.generator.populator.helper.EnsureCover;
-import cn.nukkit.level.generator.populator.helper.EnsureGrassBelow;
-import cn.nukkit.level.generator.populator.type.PopulatorSurfaceBlock;
+import cn.nukkit.level.generator.populator.helper.PopulatorHelpers;
+import cn.nukkit.level.generator.populator.type.PopulatorCount;
 import cn.nukkit.math.NukkitRandom;
 
 /**
  * @author GoodLucky777
  */
-public class PopulatorVines extends PopulatorSurfaceBlock {
+public class PopulatorVines extends PopulatorCount {
 
     @Override
-    protected boolean canStay(int x, int y, int z, FullChunk chunk) {
-        return EnsureCover.ensureCover(x, y, z, chunk) && (EnsureGrassBelow.ensureGrassBelow(x, y, z, chunk) || EnsureBelow.ensureBelow(x, y, z, DIRT, chunk) || EnsureBelow.ensureBelow(x, y, z, PODZOL, chunk));
-    }
-    
-    @Override
-    protected int getBlockId(int x, int z, NukkitRandom random, FullChunk chunk) {
-        return VINE << Block.DATA_BITS;
+    protected int getHighestWorkableBlock(ChunkManager level, int x, int z, FullChunk chunk) {
+        int y;
+        for (y = 254; y >= 64; --y) { // Vines don't generate under Y 64
+            if (!(PopulatorHelpers.isNonSolid(chunk.getBlockId(x - 1, y, z)) || PopulatorHelpers.isNonSolid(chunk.getBlockId(x + 1, y, z)) || PopulatorHelpers.isNonSolid(chunk.getBlockId(x, y, z - 1)) || PopulatorHelpers.isNonSolid(chunk.getBlockId(x, y, z + 1)) || PopulatorHelpers.isNonSolid(chunk.getBlockId(x, y + 1, z)))) {
+                break;
+            }
+        }
+        
+        return y == 0 ? -1 : ++y;
     }
     
     @Override
