35e06a08ba1525f79787bed2ae8fdfae70626132
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-09 17:27:08 +0900

    Create PopulatorBamboo.java

---
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorBamboo.java
@@ -0,0 +1,31 @@
+package cn.nukkit.level.generator.populator.impl;
+
+import cn.nukkit.block.Block;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.level.generator.populator.helper.EnsureBelow;
+import cn.nukkit.level.generator.populator.helper.EnsureCover;
+import cn.nukkit.level.generator.populator.helper.EnsureGrassBelow;
+import cn.nukkit.level.generator.populator.type.PopulatorSurfaceBlock;
+import cn.nukkit.math.NukkitRandom;
+
+/**
+ * @author Good777LUCKY
+ */
+public class PopulatorBamboo extends PopulatorSurfaceBlock {
+
+    @Override
+    protected boolean canStay(int x, int y, int z, FullChunk chunk) {
+        return EnsureCover.ensureCover(x, y, z, chunk) && (EnsureGrassBelow.ensureGrassBelow(x, y, z, chunk) || EnsureBelow.ensureBelow(x, y, z, PODZOL, chunk));
+    }
+    
+    @Override
+    protected int getBlockId(int x, int z, NukkitRandom random, FullChunk chunk) {
+        return BAMBOO << Block.DATA_BITS;
+    }
+    
+    @Override
+    protected void placeBlock(int x, int y, int z, int id, FullChunk chunk, NukkitRandom random) {
+        
+    }
+}
