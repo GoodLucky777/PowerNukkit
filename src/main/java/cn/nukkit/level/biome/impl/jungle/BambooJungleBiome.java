2472a402a52d421a3c20f856992cc7c73f702738
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-10 22:17:34 +0900

    fix?

--- src/main/java/cn/nukkit/level/biome/impl/jungle/BambooJungleBiome.java
+++ src/main/java/cn/nukkit/level/biome/impl/jungle/BambooJungleBiome.java
@@ -13,13 +13,13 @@ public class BambooJungleBiome extends JungleBiome {
 
     public BambooJungleBiome() {
         PopulatorBamboo bamboo = new PopulatorBamboo();
-        bamboo.setBaseAmount(20);
-        bamboo.setRandomAmount(25);
+        bamboo.setBaseAmount(30);
+        bamboo.setRandomAmount(15);
         this.addPopulator(bamboo);
         
         JungleBigTreePopulator jungleBigTrees = new JungleBigTreePopulator();
-        jungleBigTrees.setBaseAmount(1);
-        jungleBigTrees.setRandomAmount(2);
+        jungleBigTrees.setBaseAmount(0);
+        jungleBigTrees.setRandomAmount(1);
         this.addPopulator(jungleBigTrees);
         
         /*PopulatorTree oakBigTree = new PopulatorTree(BlockSapling.OAK); // TODO: Should be only Fancy (Big) Oak Tree
@@ -28,8 +28,8 @@ public class BambooJungleBiome extends JungleBiome {
         this.addPopulator(oakBigTree);*/
         
         PopulatorVines vines = new PopulatorVines();
-        vines.setBaseAmount(3);
-        vines.setRandomAmount(2);
+        vines.setBaseAmount(5);
+        vines.setRandomAmount(3);
         this.addPopulator(vines);
     }
     
