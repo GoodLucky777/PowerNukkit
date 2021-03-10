f873e6dde2f94d0bc86a9c1a1d61437d63cfb9f3
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-10 21:46:58 +0900

    add tall grass to ensure cover

--- src/main/java/cn/nukkit/level/generator/populator/helper/EnsureCover.java
+++ src/main/java/cn/nukkit/level/generator/populator/helper/EnsureCover.java
@@ -11,6 +11,6 @@ import static cn.nukkit.block.BlockID.SNOW_LAYER;
 public interface EnsureCover {
     static boolean ensureCover(int x, int y, int z, FullChunk chunk)    {
         int id = chunk.getBlockId(x, y, z);
-        return id == AIR || id == SNOW_LAYER;
+        return id == AIR || id == SNOW_LAYER || id == TALL_GRASS;
     }
 }
