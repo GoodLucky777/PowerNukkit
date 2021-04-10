0d08935866120af8b6043f41883e0a9cf61baeba
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-04-10 18:21:32 +0900

    fix

--- src/main/java/cn/nukkit/block/goodlucky/BlockOreRuby.java
+++ src/main/java/cn/nukkit/block/goodlucky/BlockOreRuby.java
@@ -1,7 +1,7 @@
 package cn.nukkit.block;
 
 import cn.nukkit.item.Item;
-import cn.nukkit.item.ItemDiamond;
+import cn.nukkit.item.ItemRuby;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.item.enchantment.Enchantment;
 import cn.nukkit.math.NukkitRandom;
