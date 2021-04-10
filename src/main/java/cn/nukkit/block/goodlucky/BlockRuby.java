dac3cefdbf99983debdf3beeea14dbcbbc885ade
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-04-10 18:12:18 +0900

    Create BlockRuby.java

---
+++ src/main/java/cn/nukkit/block/goodlucky/BlockRuby.java
@@ -0,0 +1,54 @@
+package cn.nukkit.block;
+
+import cn.nukkit.item.ItemTool;
+import cn.nukkit.utils.BlockColor;
+
+/**
+ * @author GoodLucky777
+ */
+public class BlockRuby extends BlockSolid {
+
+    public BlockRuby() {
+        // Does Nothing
+    }
+    
+    @Override
+    public double getHardness() {
+        return 5;
+    }
+    
+    @Override
+    public double getResistance() {
+        return 30;
+    }
+    
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_PICKAXE;
+    }
+    
+    @Override
+    public int getId() {
+        return RUBY_BLOCK;
+    }
+    
+    @Override
+    public String getName() {
+        return "Ruby Block";
+    }
+    
+    @Override
+    public int getToolTier() {
+        return ItemTool.TIER_IRON;
+    }
+    
+    @Override
+    public BlockColor getColor() {
+        return BlockColor.RED_BLOCK_COLOR;
+    }
+    
+    @Override
+    public boolean canHarvestWithHand() {
+        return false;
+    }
+}
