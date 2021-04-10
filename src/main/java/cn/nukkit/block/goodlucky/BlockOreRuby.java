acb3584fcb42de45abea6c783762b356a164af58
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-04-10 20:36:21 +0900

    bad hack

--- src/main/java/cn/nukkit/block/goodlucky/BlockOreRuby.java
+++ src/main/java/cn/nukkit/block/goodlucky/BlockOreRuby.java
@@ -84,4 +84,9 @@ public class BlockOreRuby extends BlockSolid {
     public boolean canSilkTouch() {
         return true;
     }
+    
+    @Override
+    public int getRuntimeId() {
+        return 5411;
+    }
 }
