b77c202df2601ed2e2d0a75f12468d29697b3b56
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-04-17 19:45:42 +0900

    fix

--- src/main/java/cn/nukkit/inventory/stack/request/StackRequestSlotInfo.java
+++ src/main/java/cn/nukkit/inventory/stack/request/StackRequestSlotInfo.java
@@ -1,11 +1,13 @@
 package cn.nukkit.inventory.stack.request;
 
+import cn.nukkit.inventory.stack.ContainerSlotType;
+
 /**
  * @author GoodLucky777
  */
 public class StackRequestSlotInfo {
 
-    public int container;
+    public ContainerSlotType container;
     public byte count;
     public int stackNetworkId;
 }
