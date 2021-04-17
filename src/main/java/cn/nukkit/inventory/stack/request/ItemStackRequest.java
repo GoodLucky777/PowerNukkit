5ff97b5bf246fe99df108140c405accd17d77975
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-04-17 18:03:29 +0900

    fix

--- src/main/java/cn/nukkit/inventory/stack/request/ItemStackRequest.java
+++ src/main/java/cn/nukkit/inventory/stack/request/ItemStackRequest.java
@@ -1,11 +1,13 @@
 package cn.nukkit.inventory.stack.request;
 
+import cn.nukkit.inventory.stack.request.action.StackRequestAction;
+
 /**
  * @author GoodLucky777
  */
 public class ItemStackRequest {
 
     public int requestId;
-    public StackRequestActionData[] actions;
+    public StackRequestAction[] actions;
     public String[] filterStrings;
 }
