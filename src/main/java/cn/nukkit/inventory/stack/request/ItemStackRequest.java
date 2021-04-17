9e10722cc7da5cf977211d7a7721ef7d51d636e6
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-04-17 17:57:13 +0900

    Create ItemStackRequest.java

---
+++ src/main/java/cn/nukkit/inventory/stack/request/ItemStackRequest.java
@@ -0,0 +1,11 @@
+package cn.nukkit.inventory.stack.request;
+
+/**
+ * @author GoodLucky777
+ */
+public class ItemStackRequest {
+
+    public int requestId;
+    public StackRequestActionData[] actions;
+    public String[] filterStrings;
+}
