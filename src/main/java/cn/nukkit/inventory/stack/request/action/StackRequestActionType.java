baf99d7f43333941940bd048b54b1bebb1966f8e
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-04-17 18:50:51 +0900

    Create StackRequestActionType.java

---
+++ src/main/java/cn/nukkit/inventory/stack/request/action/StackRequestActionType.java
@@ -0,0 +1,24 @@
+package cn.nukkit.inventory.stack.request.action;
+
+/**
+ * @author GoodLucky777
+ */
+public enum StackRequestActionType {
+
+    TAKE,
+    PLACE,
+    SWAP,
+    DROP,
+    DESTROY,
+    CONSUME,
+    CREATE,
+    LAB_TABLE_COMBINE,
+    BEACON_PAYMENT,
+    MINE_BLOCK,
+    CRAFT_RECIPE,
+    CRAFT_RECIPE_AUTO,
+    CRAFT_CREATIVE,
+    CRAFT_RECIPE_OPTIONAL,
+    CRAFT_NON_IMPLEMENTED_DEPRECATED,
+    CRAFT_RESULTS_DEPRECATED;
+}
