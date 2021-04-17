303f214168000c4e3a075b4000c7535aad138289
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-04-17 20:12:08 +0900

    update

--- src/main/java/cn/nukkit/network/protocol/ItemStackResponsePacket.java
+++ src/main/java/cn/nukkit/network/protocol/ItemStackResponsePacket.java
@@ -2,22 +2,34 @@ package cn.nukkit.network.protocol;
 
 import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.Since;
+import cn.nukkit.inventory.stack.response.ItemStackResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.ToString;
 
 /**
  * @author joserobjr
+ * @author GoodLucky777
  */
 @PowerNukkitOnly
 @Since("1.4.0.0-PN")
 @ToString
 public class ItemStackResponsePacket extends DataPacket {
+
     @PowerNukkitOnly
     @Since("1.4.0.0-PN")
     public static final byte NETWORK_ID = ProtocolInfo.ITEM_STACK_RESPONSE_PACKET;
-
+    
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    public List<ItemStackResponse> responses = new ArrayList<>();
+    
     @Override
     public void encode() {
-        throw new UnsupportedOperationException(); //TODO
+        this.reset();
+        
     }
 
     @Override
