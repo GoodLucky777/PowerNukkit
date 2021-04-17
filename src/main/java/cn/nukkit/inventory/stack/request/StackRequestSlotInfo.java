7f6b76ce92385b1583ad2ef11706a49b0b084296
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-04-17 19:21:53 +0900

    Create StackRequestSlotInfo.java

---
+++ src/main/java/cn/nukkit/inventory/stack/request/StackRequestSlotInfo.java
@@ -0,0 +1,11 @@
+package cn.nukkit.inventory.stack.request;
+
+/**
+ * @author GoodLucky777
+ */
+public class StackRequestSlotInfo {
+
+    public int container;
+    public byte count;
+    public int stackNetworkId;
+}
