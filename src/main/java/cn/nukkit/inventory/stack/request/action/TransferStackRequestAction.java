a40b54043d1631bdc846432d7623dd440eba69f7
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-04-17 19:57:01 +0900

    fix

--- src/main/java/cn/nukkit/inventory/stack/request/action/TransferStackRequestAction.java
+++ src/main/java/cn/nukkit/inventory/stack/request/action/TransferStackRequestAction.java
@@ -6,10 +6,4 @@ import cn.nukkit.inventory.stack.request.StackRequestSlotInfo;
  * @author GoodLucky777
  */
 public interface TransferStackRequestAction extends StackRequestAction {
-
-    byte getCount();
-    
-    StackRequestSlotInfo getSource();
-    
-    StackRequestSlotInfo getDestination();
 }
