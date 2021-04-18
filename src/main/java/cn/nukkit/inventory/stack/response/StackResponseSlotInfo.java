e394ec8bbf5c1b53ea0d562837012fd55de721b1
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-04-18 10:04:50 +0900

    Create StackResponseSlotInfo.java

---
+++ src/main/java/cn/nukkit/inventory/stack/response/StackResponseSlotInfo.java
@@ -0,0 +1,14 @@
+package cn.nukkit.inventory.stack.response;
+
+/**
+ * @author GoodLucky777
+ */
+public class StackResponseSlotInfo {
+
+    public byte slot;
+    public byte hotbarSlot;
+    public byte count;
+    public int stackNetworkId;
+    public String customName;
+    public int durabilityCorrection;
+}
