30a8bf96258ada33551eee1d46aafaebd80965e7
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-04-18 10:02:06 +0900

    Create StackResponseContainerInfo.java

---
+++ src/main/java/cn/nukkit/inventory/stack/response/StackResponseContainerInfo.java
@@ -0,0 +1,14 @@
+package cn.nukkit.inventory.stack.response;
+
+import cn.nukkit.inventory.stack.ContainerSlotType;
+
+import java.util.List;
+
+/**
+ * @author GoodLucky777
+ */
+public class StackResponseContainerInfo {
+
+    public ContainerSlotType container;
+    public List<StackResponseSlotInfo> slots;
+}
