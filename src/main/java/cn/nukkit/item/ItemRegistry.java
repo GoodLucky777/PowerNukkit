064dbe0c7e1954f00aa78b1202c12062a1e856f9
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-05-30 21:38:18 +0900

    .

--- src/main/java/cn/nukkit/item/ItemRegistry.java
+++ src/main/java/cn/nukkit/item/ItemRegistry.java
@@ -14,7 +14,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java,util.List;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import javax.annotation.ParametersAreNonnullByDefault;
 
