b3bd592bf65b044fb5e586029bb5c196765a95a0
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-05-30 17:24:42 +0900

    .

--- src/main/java/cn/nukkit/item/ItemRegistry.java
+++ src/main/java/cn/nukkit/item/ItemRegistry.java
@@ -2,12 +2,23 @@ package cn.nukkit.item;
 
 import cn.nukkit.utils.Identifier;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import lombok.extern.log4j.Log4j2;
+
 /**
  * @author GoodLucky777
  */
+@Log4j2
+@ParametersAreNonnullByDefault
 public class ItemRegistry {
 
     private static final ItemRegistry instance;
+    
+    private BiMap<Identifier, Item> itemRegisteration = HashBiMap.create();
     
     public ItemRegistry() {
         this.registerVanilla();
@@ -17,7 +28,7 @@ public class ItemRegistry {
         return instance;
     }
     
-    public void registerItem(Item item, Identifier identifier) {
+    public void registerItem(Identifier identifier, Item item) {
         
     }
     
