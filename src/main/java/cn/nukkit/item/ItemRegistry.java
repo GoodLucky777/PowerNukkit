3ea3214728fce99e42d5b3e7bad6be409edf7623
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-05-30 18:11:12 +0900

    .

--- src/main/java/cn/nukkit/item/ItemRegistry.java
+++ src/main/java/cn/nukkit/item/ItemRegistry.java
@@ -19,9 +19,11 @@ public class ItemRegistry {
 
     private static final ItemRegistry instance;
     
+    private BiMap<Integer, Identifier> legacyIdRegistration = HashBiMap.create();
     private BiMap<Identifier, Item> itemRegisteration = HashBiMap.create();
     
     public ItemRegistry() {
+        this.loadItemIds();
         this.registerVanilla();
         
         instance = this;
@@ -29,6 +31,35 @@ public class ItemRegistry {
     
     public static ItemRegistry getInstance() {
         return instance;
+    }
+    
+    private void loadItemIds() {
+        try (InputStream stream = Server.class.getClassLoader().getResourceAsStream("item_ids.csv")) { 
+            if (stream == null) {
+                throw new AssertionError("Unable to find item_ids.csv");
+            }
+            
+            int count = 0;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    count++;
+                    line = line.trim();
+                    if (line.isEmpty()) {
+                        continue;
+                    }
+                    String[] parts = line.split(",");
+                    Preconditions.checkArgument(parts.length == 2 || parts[0].matches("^[0-9]+$"));
+                    if (parts.length > 1) {
+                        legacyIdRegistration.put(Integer.parseInt(parts[0]), Identifier.fromString(parts[1]));
+                    }
+                }
+            } catch (Exception e) {
+                throw new IOException("Error reading the line " + count + " of the item_ids.csv", e);
+            }
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
     }
     
     public synchronized void registerItem(Identifier identifier, Item item) {
