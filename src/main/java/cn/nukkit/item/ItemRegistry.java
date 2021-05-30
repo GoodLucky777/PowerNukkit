c28b987dc4c65b1296fa280a0a64e593242b476d
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-05-30 16:43:45 +0900

    Create ItemRegistry.java

---
+++ src/main/java/cn/nukkit/item/ItemRegistry.java
@@ -0,0 +1,25 @@
+package cn.nukkit.item;
+
+/**
+ * @author GoodLucky777
+ */
+public class ItemRegistry {
+
+    private static final ItemRegistry instance;
+    
+    public ItemRegistry() {
+        this,registerVanilla();
+    }
+    
+    public static getInstance() {
+        return instance;
+    }
+    
+    public void registerItem() {
+        
+    }
+    
+    public void registerVanilla() {
+        
+    }
+}
