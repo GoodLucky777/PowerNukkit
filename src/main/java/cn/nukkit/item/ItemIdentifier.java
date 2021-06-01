689b94bacdc90fe1fea6d595c23978cd8becf7b0
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-01 14:41:46 +0900

    .

--- src/main/java/cn/nukkit/item/ItemIdentifier.java
+++ src/main/java/cn/nukkit/item/ItemIdentifier.java
@@ -4,10 +4,14 @@ import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.Since;
 import cn.nukkit.utils.Identifier;
 
+/**
+ * @author GoodLucky777
+ */
 public interface ItemIdentifier {
     Identifier IRON_SHOVEL = Identifier.fromString("iron_shovel");
     Identifier IRON_PICKAXE = Identifier.fromString("iron_pickaxe");
     Identifier IRON_AXE = Identifier.fromString("iron_axe");
     Identifier FLINT_AND_STEEL = Identifier.fromString("flint_and_steel");
     Identifier APPLE = Identifier.fromString("apple");
+    
 }
