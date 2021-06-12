8dbe8f4aeb5f7c1f9033f30dcda078e6d7f69202
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-12 19:47:03 +0900

    fix import

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorOreV2.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorOreV2.java
@@ -7,6 +7,7 @@ import cn.nukkit.level.ChunkManager;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.level.generator.object.ore.OreV2;
 import cn.nukkit.level.generator.populator.type.Populator;
+import cn.nukkit.math.MathHelper;
 import cn.nukkit.math.NukkitMath;
 import cn.nukkit.math.NukkitRandom;
 
