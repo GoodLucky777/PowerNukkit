cb1485043fc0bc97167964ed2710f86ba55ae0bc
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-16 10:59:47 +0900

    test

--- src/main/java/cn/nukkit/Server.java
+++ src/main/java/cn/nukkit/Server.java
@@ -2192,7 +2192,7 @@ public class Server {
         if (generator == null) {
             generator = Generator.getGenerator(this.getLevelType());
         }
-        
+        log.info(generator.getName());
         if (generator.getName().equals("PowerNukkitOverworld")) {
             log.warn(TextFormat.RED + "You are using PowerNukkit's experimental world generator! If you use it to the original world from Cloudburst Nukkit, the world will be corrupted. Also, there may be a breaking change in the future.");
         }
