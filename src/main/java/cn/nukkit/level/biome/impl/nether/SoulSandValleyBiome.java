bb742b0fc8ef6247699447da6fb4fec984d67013
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-16 18:30:25 +0900

    Create SoulSandValleyBiome.java

---
+++ src/main/java/cn/nukkit/level/biome/impl/nether/SoulSandValleyBiome.java
@@ -0,0 +1,18 @@
+package cn.nukkit.level.biome.impl;
+
+import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.Since;
+import cn.nukkit.level.biome.impl.HellBiome;
+
+/**
+ * @author GoodLucky777
+ */
+@PowerNukkitOnly
+@Since("FUTURE")
+public class SoulSandValleyBiome extends HellBiome {
+
+    @Override
+    public String getName() {
+        return "Soul Sand Valley";
+    }
+}
