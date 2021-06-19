9a9ba70e541126ed5b20c2d924fac24e9d7b2822
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-19 20:45:17 +0900

    fix

--- src/main/java/cn/nukkit/utils/Cubiomes.java
+++ src/main/java/cn/nukkit/utils/Cubiomes.java
@@ -1,6 +1,5 @@
 package cn.nukkit.utils;
 
-import cn.nukkit.Server;
 import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.Since;
 
@@ -15,7 +14,7 @@ import com.sun.jna.Native;
 public class Cubiomes {
 
     public interface CLibrary extends Library {
-        CLibrary INSTANCE = (CLibrary) Native.loadLibrary(Server.class.getClassLoader().getResource("lib/libcubiomes.so").getPath(), CLibrary.class);
+        CLibrary INSTANCE = (CLibrary) Native.loadLibrary("lib/libcubiomes.so", CLibrary.class);
         
         int genNetherScaled(int mc, long seed, int scale, int[] out, int x, int z, int w, int h, int y0, int y1);
     }
