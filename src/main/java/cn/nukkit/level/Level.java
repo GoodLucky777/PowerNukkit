18d3a2f803a74fbb9677f55453c650d0d6f5e24d
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-19 18:42:06 +0900

    Add warning log

--- src/main/java/cn/nukkit/level/Level.java
+++ src/main/java/cn/nukkit/level/Level.java
@@ -339,6 +339,9 @@ public class Level implements ChunkManager, Metadatable {
                 TextFormat.GREEN + levelProvider.getName() + TextFormat.WHITE));
 
         this.generatorClass = Generator.getGenerator(levelProvider.getGenerator());
+        if (generatorClass.getSimpleName().equals("PowerNukkitNether")) {
+            log.warn(TextFormat.RED + "You are using PowerNukkit's experimental world generator! If you use it to the original world from Cloudburst Nukkit, the world will be corrupted. Also, there may be a breaking change in the future.");
+        }
 
         this.useSections = usesChunkSection.getAsBoolean();
 
