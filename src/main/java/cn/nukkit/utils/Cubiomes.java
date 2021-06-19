4bd2eb858b57ac117d546054a262b5ce6aca629e
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-19 16:54:04 +0900

    Create Cubiomes.java

---
+++ src/main/java/cn/nukkit/utils/Cubiomes.java
@@ -0,0 +1,25 @@
+package cn.nukkit.utils;
+
+import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.Since;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+/**
+ * @author GoodLucky777
+ */
+@PowerNukkitOnly
+@Since("FUTURE")
+public class Cubiomes {
+
+    public interface CLibrary extends Library {
+        CLibrary INSTANCE = (CLibrary) Native.load(("cubiomes"), CLibrary.class);
+        
+        int genNetherScaled(int mc, long seed, int scale, int[] out, int x, int z, int w, int h, int y0, int y1);
+    }
+    
+    public static CLibrary getInstance() {
+        return INSTANCE;
+    }
+}
