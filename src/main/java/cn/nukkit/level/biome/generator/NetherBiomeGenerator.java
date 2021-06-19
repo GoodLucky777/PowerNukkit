e34ae01d3f079013db7ce7c1241a20456dd286e4
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-19 16:58:55 +0900

    Create NetherBiomeGenerator.java

---
+++ src/main/java/cn/nukkit/level/biome/generator/NetherBiomeGenerator.java
@@ -0,0 +1,27 @@
+package cn.nukkit.level.biome;
+
+import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.Since;
+import cn.nukkit.utils.Cubiomes;
+
+/**
+ * @author GoodLucky777
+ */
+@PowerNukkitOnly
+@Since("FUTURE")
+public class NetherBiomeGenerator {
+
+    private static final int CURRENT_VERSION = 17;
+    
+    private long seed;
+    
+    public NetherBiomeGenerator(long seed) {
+        this.seed = seed;
+    }
+    
+    public int[] getNetherBiomeIds(int chunkX, int chunkZ) {
+        int[] ids = new int[16 * 16];
+        Cubiomes.getInstance().genNetherScaled(CURRENT_VERSION, seed, 1, ids, chunkX, chunkZ, 16, 16, 0, 0);
+        return ids;
+    }
+}
