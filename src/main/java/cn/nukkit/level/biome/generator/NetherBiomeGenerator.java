91501c011812643c358416207a734dbe39ec5a43
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-19 19:00:36 +0900

    fix

--- src/main/java/cn/nukkit/level/biome/generator/NetherBiomeGenerator.java
+++ src/main/java/cn/nukkit/level/biome/generator/NetherBiomeGenerator.java
@@ -4,6 +4,8 @@ import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.Since;
 import cn.nukkit.utils.Cubiomes;
 
+import com.google.common.collect.ImmutableList;
+
 /**
  * @author GoodLucky777
  */
@@ -12,6 +14,13 @@ import cn.nukkit.utils.Cubiomes;
 public class NetherBiomeGenerator {
 
     private static final int CURRENT_VERSION = 17;
+    
+    private static ImmutableMap<Integer, Integer> BIOME_ID_MAP = ImmutableMap.<Integer, Integer>builder()
+        .put(8, 8)
+        .put(170, 178)
+        .put(171, 179)
+        .put(172, 180)
+        .put(173, 181);
     
     private long seed;
     
@@ -23,5 +32,9 @@ public class NetherBiomeGenerator {
         int[] ids = new int[16 * 16];
         Cubiomes.getInstance().genNetherScaled(CURRENT_VERSION, seed, 1, ids, chunkX, chunkZ, 16, 16, 0, 0);
         return ids;
+    }
+    
+    public static int convertBiomeId(int javaId) {
+        return BIOME_ID_MAP.get(javaId);
     }
 }
