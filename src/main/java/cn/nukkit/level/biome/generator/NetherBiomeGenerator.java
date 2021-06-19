ed8c8d0d77b829e1be788f5b49251db4039605a8
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-19 19:33:21 +0900

    fix

--- src/main/java/cn/nukkit/level/biome/generator/NetherBiomeGenerator.java
+++ src/main/java/cn/nukkit/level/biome/generator/NetherBiomeGenerator.java
@@ -4,8 +4,8 @@ import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.Since;
 import cn.nukkit.utils.Cubiomes;
 
-import it.unimi.dsi.fastutil.ints.Int2IntHashMap;
 import it.unimi.dsi.fastutil.ints.Int2IntMap;
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 
 /**
  * @author GoodLucky777
@@ -16,7 +16,7 @@ public class NetherBiomeGenerator {
 
     private static final int CURRENT_VERSION = 17;
     
-    private static final Int2IntMap BIOME_ID_MAP = new Int2IntHashMap()
+    private static final Int2IntMap BIOME_ID_MAP = new Int2IntOpenHashMap()
         .put(8, 8)
         .put(170, 178)
         .put(171, 179)
