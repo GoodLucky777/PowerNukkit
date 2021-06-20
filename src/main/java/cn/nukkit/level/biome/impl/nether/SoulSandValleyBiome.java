b6df680c7671416f6a4fd6cdca197a36019af3c9
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-20 11:23:53 +0900

    fix

--- src/main/java/cn/nukkit/level/biome/impl/nether/SoulSandValleyBiome.java
+++ src/main/java/cn/nukkit/level/biome/impl/nether/SoulSandValleyBiome.java
@@ -2,6 +2,7 @@ package cn.nukkit.level.biome.impl.nether;
 
 import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.Since;
+import cn.nukkit.blockstate.BlockState;
 import cn.nukkit.level.biome.impl.HellBiome;
 
 /**
@@ -11,8 +12,26 @@ import cn.nukkit.level.biome.impl.HellBiome;
 @Since("FUTURE")
 public class SoulSandValleyBiome extends HellBiome {
 
+    private static final BlockState STATE_SOULSAND = BlockState.of(SOUL_SAND);
+    private static final BlockState STATE_SOULSOIL = BlockState.of(SOULSOIL);
+    
     @Override
     public String getName() {
         return "Soul Sand Valley";
+    }
+    
+    @Override
+    public BlockState getGroundState() {
+        return STATE_SOULSAND;
+    }
+    
+    @Override
+    public BlockState getTopStateGravelNoise() {
+        return STATE_SOULSOIL;
+    }
+    
+    @Override
+    public BlockState getTopStateSoulsandNoise() {
+        return STATE_SOULSOIL;
     }
 }
