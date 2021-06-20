ba9ded815e16443caf855bd83d4464766bf2d4b1
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-20 10:19:11 +0900

    update

--- src/main/java/cn/nukkit/level/biome/impl/HellBiome.java
+++ src/main/java/cn/nukkit/level/biome/impl/HellBiome.java
@@ -2,9 +2,14 @@ package cn.nukkit.level.biome.impl;
 
 import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.Since;
+import cn.nukkit.blockstate.BlockState;
 import cn.nukkit.level.biome.Biome;
 
 public class HellBiome extends Biome {
+
+    private static final BlockState STATE_LAVA = BlockState.of(STILL_LAVA);
+    private static final BlockState STATE_NETHERRACK = BlockState.of(NETHERRACK);
+    
     @Override
     public String getName() {
         return "Hell";
@@ -20,5 +25,23 @@ public class HellBiome extends Biome {
     @Override
     public boolean isDry() {
         return true;
+    }
+    
+    @PowerNukkitOnly
+    @Since("FUTURE")
+    public BlockState getSurfaceTopState() {
+        return STATE_NETHERRACK;
+    }
+    
+    @PowerNukkitOnly
+    @Since("FUTURE")
+    public BlockState getGroundState() {
+        return STATE_NETHERRACK;
+    }
+    
+    @PowerNukkitOnly
+    @Since("FUTURE")
+    public BlockState getSeaState() {
+        return STATE_LAVA;
     }
 }
