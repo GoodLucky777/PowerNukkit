1d3d0a9da2064e3b2f695c5dd7e16c972da7e555
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-20 10:11:49 +0900

    update

--- src/main/java/cn/nukkit/level/generator/PowerNukkitNether.java
+++ src/main/java/cn/nukkit/level/generator/PowerNukkitNether.java
@@ -255,12 +255,15 @@ public class PowerNukkitNether extends Generator {
         this.soulsandNoise = this.soulsandGravelNoiseGen.generateNoiseOctaves(this.soulsandNoise, baseX, baseZ, 0, 16, 16, 1, 0.03125, 0.03125, 1);
         this.gravelNoise = this.soulsandGravelNoiseGen.generateNoiseOctaves(this.gravelNoise, baseX, 109, baseZ, 16, 1, 16, 0.03125, 1, 0.03125);
 
-        int[] biomeIds = netherBiomeGenerator.generateBiomeIds(chunkX, chunkZ);cn.nukkit.utils.MainLogger.getLogger().info(java.util.Arrays.toString(biomeIds));
+        int[] biomeIds = netherBiomeGenerator.generateBiomeIds(chunkX, chunkZ); //cn.nukkit.utils.MainLogger.getLogger().info(java.util.Arrays.toString(biomeIds));
 
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
-                BlockState topState = STATE_NETHERRACK;
-                BlockState groundState = STATE_NETHERRACK;
+                Biome biome = Biome.getBiome(NetherBiomeGenerator.convertBiomeId(biomeIds[x | z << 4]));
+                chunk.setBiomeId(x, z, biome.getId());
+                
+                BlockState topState = biome.getSurfaceState();
+                BlockState groundState = biome.getGroundState();
 
                 boolean soulSand = this.soulsandNoise[x | z << 4] + this.nukkitRandom.nextDouble() * 0.2D > 0.0D;
                 boolean gravel = this.gravelNoise[x | z << 4] + this.nukkitRandom.nextDouble() * 0.2D > 0.0D;
@@ -309,8 +312,6 @@ public class PowerNukkitNether extends Generator {
                         }
                     }
                 }
-
-                chunk.setBiomeId(x, z, NetherBiomeGenerator.convertBiomeId(biomeIds[x | z << 4]));
             }
         }
 
