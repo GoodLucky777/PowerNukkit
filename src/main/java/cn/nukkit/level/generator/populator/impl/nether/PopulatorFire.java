93b9e2887142fc690d74f7e585f0bd1a5806cc9d
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-06-21 21:14:25 +0900

    fix

--- src/main/java/cn/nukkit/level/generator/populator/impl/nether/PopulatorFire.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/nether/PopulatorFire.java
@@ -20,26 +20,24 @@ public class PopulatorFire extends Populator {
     public PopulatorFire(BlockState fire, int amount, List<BlockState> belowState) {
         this.fire = fire;
         this.amount = amount;
-        this.belowBlock = belowState;
+        this.belowState = belowState;
     }
     
     @Override
     public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
+        int x = (chunkX << 4) + random.nextBoundedInt(16);
+        int z = (chunkZ << 4) + random.nextBoundedInt(16);
+        int y = random.nextBoundedInt(128);
+        
         for (int i = 0; i < amount; i++) {
-            int x = (chunkX << 4) + random.nextBoundedInt(16);
-            int z = (chunkZ << 4) + random.nextBoundedInt(16);
-            int y = random.nextBoundedInt(128);
+            x += random.nextBoundedInt(8) - random.nextBoundedInt(8);
+            z += random.nextBoundedInt(8) - random.nextBoundedInt(8);
+            y += random.nextBoundedInt(4) - random.nextBoundedInt(4);
             
-            for (int j = 0; j < 64; j++) {
-                x += random.nextBoundedInt(8) - random.nextBoundedInt(8);
-                z += random.nextBoundedInt(8) - random.nextBoundedInt(8);
-                y += random.nextBoundedInt(4) - random.nextBoundedInt(4);
-                
-                if (y < 128 && level.getBlockIdAt(x, y, z) == AIR) {
-                    for (BlockState state : this.belowState) {
-                        if (level.getBlockStateAt(x, y, z).equals(state)) {
-                            level.setBlockStateAt(x, y, z, fire);
-                        }
+            if (y < 128 && level.getBlockIdAt(x, y, z) == AIR) {
+                for (BlockState state : this.belowState) {
+                    if (level.getBlockStateAt(x, y, z).equals(state)) {
+                        level.setBlockStateAt(x, y, z, fire);
                     }
                 }
             }
