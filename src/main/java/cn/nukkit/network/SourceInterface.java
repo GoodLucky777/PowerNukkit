e5cb69342044627b70ec7ba941e5988165c28d57
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-07-02 20:28:08 +0900

    fix

--- src/main/java/cn/nukkit/network/SourceInterface.java
+++ src/main/java/cn/nukkit/network/SourceInterface.java
@@ -1,6 +1,8 @@
 package cn.nukkit.network;
 
 import cn.nukkit.Player;
+import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.Since;
 import cn.nukkit.network.protocol.DataPacket;
 
 
@@ -29,5 +31,7 @@ public interface SourceInterface {
 
     void emergencyShutdown();
     
+    @PowerNukkitOnly
+    @Since("FUTURE")
     Integer putResourcePacket(Player player, DataPacket packet);
 }
