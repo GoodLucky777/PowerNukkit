97097932bd12625fc97fed1eec75a69e392f249a
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-07-16 13:22:51 +0900

    fix import

--- src/main/java/cn/nukkit/block/BlockSnowPowder.java
+++ src/main/java/cn/nukkit/block/BlockSnowPowder.java
@@ -2,8 +2,12 @@ package cn.nukkit.block;
 
 import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.Since;
+import cn.nukkit.blockproperty.BlockProperties;
+import cn.nukkit.blockproperty.CommonBlockProperties;
 import cn.nukkit.item.Item;
 import cn.nukkit.utils.BlockColor;
+
+import javax.annotation.Nonnull;
 
 /**
  * @author GoodLucky777
